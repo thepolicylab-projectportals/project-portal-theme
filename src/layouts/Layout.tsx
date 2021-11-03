4ca0ec692c4effe0329516f141b3e70d0acfddbc
Kevin Wilson <khwilson@gmail.com>
2021-11-02 23:01:52 -0400

    Remove LayoutFull

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,17 +1,22 @@
 import React, { FunctionComponent, ReactNode } from "react"
 import "typeface-roboto"
+import { Footer, SignupForm } from "../components"
 import "../styles/style.css"
 
-import { LayoutFull } from "./LayoutFull"
-
 interface LayoutProps {
-  navigation?: any
   children: ReactNode
 }
 
-export const Layout: FunctionComponent<LayoutProps> = ({
-  children,
-  navigation,
-}: LayoutProps) => {
-  return <LayoutFull>{children}</LayoutFull>
+export const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
+  return (
+    <div className="bg-white">
+      {children}
+      <div className="bg-white dark:bg-transparent border-t border-b border-transparent py-8 lg:py-16 mt-8">
+        <div className="container">
+          <SignupForm />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
 }
