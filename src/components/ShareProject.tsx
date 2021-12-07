15f34ae5b5279691790ba1995b8c8e7567fa7179
digicosmos86 <digicosmos@gmail.com>
2021-12-07 10:06:50 -0500

    Updated share buttons and date formats

--- src/components/ShareProject.tsx
+++ src/components/ShareProject.tsx
@@ -13,22 +13,24 @@ import {
 } from "react-share"
 
 export const ShareProject: React.FC = () => {
+  const url = typeof window !== "undefined" ? window.location.href : ""
+
   return (
     <div className="mt-8 w-72 lg:mt-0">
       <h4 className="font-bold text-white">Share this project</h4>
       <div className="flex gap-1 mt-1 flex-nowrap place-content-start">
-        <FacebookShareButton url={window.location.href} resetButtonStyle={true}>
+        <FacebookShareButton url={url} resetButtonStyle={true}>
           <FaFacebookSquare className="text-2xl text-white" />
         </FacebookShareButton>
-        <TwitterShareButton url={window.location.href} resetButtonStyle={true}>
+        <TwitterShareButton url={url} resetButtonStyle={true}>
           <FaTwitterSquare className="text-2xl text-white" />
         </TwitterShareButton>
-        <LinkedinShareButton url={window.location.href} resetButtonStyle={true}>
+        <LinkedinShareButton url={url} resetButtonStyle={true}>
           <FaLinkedin className="text-2xl text-white" />
         </LinkedinShareButton>
         <EmailShareButton
           resetButtonStyle={true}
-          url={window.location.href}
+          url={url}
           subject={"Sharing a wonderful project opportunity!"}
         >
           <FaEnvelopeSquare className="text-2xl text-white" />
