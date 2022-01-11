cf1952e4af58ef1086c008971d0f292884f465d5
Mary McGrath <m.c.mcgrath13@gmail.com>
2022-01-11 10:55:13 -0500

    ci: maybe a colors instead of a color

--- sites/satx/tailwind.config.js
+++ sites/satx/tailwind.config.js
@@ -1,9 +1,11 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   presets: [require("../../tailwind.config.js")],
   theme: {
     extend: {
       colors: {
-        blue: "green",
+        blue: colors.fuchsia,
       },
     },
   },
