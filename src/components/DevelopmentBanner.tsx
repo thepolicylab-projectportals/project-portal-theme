3007ede52bb3520b667d1a5fcee807cb06590c4a
Mary McGrath <m.c.mcgrath13@gmail.com>
2022-02-16 15:38:07 -0500

    feat: under construction banner when site not live yet

---
+++ src/components/DevelopmentBanner.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const DevelopmentBanner: React.FC = () => {
+  return (
+    <div className="py-6 overflow-hidden bg-warning text-center">
+      <h4>⚠️This site is still under construction⚠️</h4>
+    </div>
+  )
+}
