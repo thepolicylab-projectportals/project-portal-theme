567ef42e9016e0fb1066b6b2c39d959fbc60a528
jashlu <joshlu9@gmail.com>
2022-02-17 14:34:23 -0500

    refactor: introduce higher pixel icon

--- src/templates/ProjectDetail.tsx
+++ src/templates/ProjectDetail.tsx
@@ -199,8 +199,7 @@ const ProjectDetail: FunctionComponent<ProjectDetailProps> = (props) => {
           <section className="my-12">
             <Link to={withPrefix(`/${status === "open" ? "" : status}`)}>
               <button className="btn m-responsive">
-                {" "}
-                <StaticImage class="mt-0.5" src="../images/backarrow.png" />
+                <StaticImage class="mt-1 w-2" src="../images/backarrow.png" />
                 <span class="pl-2">Back</span>
               </button>
             </Link>
