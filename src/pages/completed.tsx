9eb501239ce64e7bb04da54974390bf67d3bb4cd
Kevin Wilson <khwilson@gmail.com>
2022-02-21 12:31:47 -0500

    Update default date terms

--- src/pages/completed.tsx
+++ src/pages/completed.tsx
@@ -39,6 +39,7 @@ export const query = graphql`
           priorResearch
           fundingInfo
           collaborationType
+          lastModified
         }
       }
     }
--- src/pages/ongoing.tsx
+++ src/pages/ongoing.tsx
@@ -40,6 +40,7 @@ export const query = graphql`
           priorResearch
           fundingInfo
           collaborationType
+          lastModified
         }
       }
     }
