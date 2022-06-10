24c7fc3946435fced6f735dd376d7c656c9b3adc
John Gerrard Holland <office@jgholland.de>
2022-06-10 15:45:07 -0400

    fix: ensure we're using site-metadata from the static queyr in the header

--- project-portal-theme/src/components/SiteMetadata.jsx
+++ project-portal-theme/src/components/SiteMetadata.jsx
@@ -1,23 +1,17 @@
 import React from "react"
-import { Helmet } from "react-helmet"
-import { graphql, useStaticQuery } from "gatsby"
+import {Helmet} from "react-helmet"
 import PropTypes from "prop-types"
+import useSiteMetadata from "../hooks/useSiteMetadata"
 
-export const SiteMetadata = ({ description, image, title }) => {
+export const SiteMetadata = ({description, image, title}) => {
   const {
-    site: {
-      siteMetadata: { locale, siteTitle },
-    },
-  } = useStaticQuery(graphql`
-    query SiteMetadata {
-      site {
-        siteMetadata {
-          locale
-          siteTitle: title
-        }
-      }
-    }
-  `)
+    siteTitle,
+    short_name,
+    siteUrl,
+    projectInterestLink,
+    live,
+    locale
+  } = useSiteMetadata()
 
   return (
     <Helmet
@@ -26,17 +20,17 @@ export const SiteMetadata = ({ description, image, title }) => {
       title={title}
       titleTemplate={`${siteTitle}`}
     >
-      <html lang={locale} />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <meta name="description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:locale" content={locale} />
-      <meta property="og:site_name" content={siteTitle} />
-      <meta property="og:image" content={image} />
-      <meta property="og:description" content={description} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:image" content={image} />
-      <meta property="twitter:description" content={description} />
+      <html lang={locale}/>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+      <meta name="description" content={description}/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:locale" content={locale}/>
+      <meta property="og:site_name" content={siteTitle}/>
+      <meta property="og:image" content={image}/>
+      <meta property="og:description" content={description}/>
+      <meta name="twitter:card" content="summary"/>
+      <meta name="twitter:image" content={image}/>
+      <meta property="twitter:description" content={description}/>
     </Helmet>
   )
 }
