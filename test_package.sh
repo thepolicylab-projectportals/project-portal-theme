b76361691aeffb9c251dccac269bb2069fa7fe2e
John Gerrard Holland <john_holland1@brown.edu>
2022-07-18 16:57:27 -0400

    fix: extend test packaging script to run faster

--- test_package.sh
+++ test_package.sh
@@ -4,8 +4,14 @@ testDir="./testDir"
 siteDir="./project-portal-example"
 themeDir="./project-portal-theme"
 
+echo "Delete the old site"
+# The rm command takes a little while so
+# to speed things up we move the directory first:
+mv $testDir .$testDir-delete
+# ... and then delete it in the background.
+(rm -rf .$testDir-delete) &
+
 echo "Initialize the new site"
-rm -rf $testDir
 rsync -av --progress $siteDir/. $testDir --exclude node_modules --exclude .cache --exclude public
 
 echo "Package the theme"
