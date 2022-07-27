d93fd687b44ae86af8771a81c994b1fa53686221
John Gerrard Holland <john_holland1@brown.edu>
2022-07-27 12:01:34 -0400

    fix: add "article" element to each card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -45,75 +45,77 @@ export const Card: FunctionComponent<CardProps> = ({
   navigation,
 }) => {
   return (
-    <div className="px-2 py-4 overflow-hidden bg-white border border-gray-200 rounded-md shadow-sm">
-      <Link to={`/project/${slug}`} state={{ navigation }}>
-        <div className="flex flex-col h-full">
-          <div className="mt-4 ml-4">
-            <ProjectStatus status={status} />
-          </div>
-          <div className="p-5 pb-5">
-            <div className="pb-4 text-h3 font-bold leading-snug text-black">
-              {question}
+    <article>
+      <div className="px-2 py-4 overflow-hidden bg-white border border-gray-200 rounded-md shadow-sm">
+        <Link to={`/project/${slug}`} state={{ navigation }}>
+          <div className="flex flex-col h-full">
+            <div className="mt-4 ml-4">
+              <ProjectStatus status={status} />
             </div>
-            <div>
-              {
-                // This code allows you to set the defaults for key dates
-                // For instance, change the second `true` here to `startDate` if you
-                // do _not_ want any date to appear when the project `startDate` is
-                // null. Note that you may also need to edit the `statusOutput` call
-                // below and also the one in `ProjectDetail.tsx`
-                statusOutput(status, true, true, true) !== null && (
-                  <div className="mt-4 text-body">
-                    <span className="font-bold">
+            <div className="p-5 pb-5">
+              <div className="pb-4 text-h3 font-bold leading-snug text-black">
+                {question}
+              </div>
+              <div>
+                {
+                  // This code allows you to set the defaults for key dates
+                  // For instance, change the second `true` here to `startDate` if you
+                  // do _not_ want any date to appear when the project `startDate` is
+                  // null. Note that you may also need to edit the `statusOutput` call
+                  // below and also the one in `ProjectDetail.tsx`
+                  statusOutput(status, true, true, true) !== null && (
+                    <div className="mt-4 text-body">
+                      <span className="font-bold">
+                        {statusOutput(
+                          status,
+                          "Opportunity closes: ",
+                          "Project started: ",
+                          "Project ended: "
+                        )}
+                      </span>
                       {statusOutput(
                         status,
-                        "Opportunity closes: ",
-                        "Project started: ",
-                        "Project ended: "
+                        opportunityCloses
+                          ? moment(opportunityCloses).format("MMMM D, YYYY")
+                          : "Open until filled",
+                        startDate
+                          ? moment(startDate).format("MMMM D, YYYY")
+                          : moment(lastModified).format("MMMM D, YYYY"),
+                        endDate
+                          ? moment(endDate).format("MMMM D, YYYY")
+                          : moment(lastModified).format("MMMM D, YYYY")
                       )}
-                    </span>
-                    {statusOutput(
-                      status,
-                      opportunityCloses
-                        ? moment(opportunityCloses).format("MMMM D, YYYY")
-                        : "Open until filled",
-                      startDate
-                        ? moment(startDate).format("MMMM D, YYYY")
-                        : moment(lastModified).format("MMMM D, YYYY"),
-                      endDate
-                        ? moment(endDate).format("MMMM D, YYYY")
-                        : moment(lastModified).format("MMMM D, YYYY")
-                    )}
-                  </div>
-                )
-              }
-              <div className="mb-4 text-body">
-                <span className="font-bold">Department or Agency: </span>
-                {agency}
-              </div>
-              {!isNA(topics) && (
-                <div className="text-tag mt-4">
-                  <Feature
-                    label="Topics"
-                    className="bg-topics"
-                    value={topics}
-                  />
+                    </div>
+                  )
+                }
+                <div className="mb-4 text-body">
+                  <span className="font-bold">Department or Agency: </span>
+                  {agency}
                 </div>
-              )}
+                {!isNA(topics) && (
+                  <div className="text-tag mt-4">
+                    <Feature
+                      label="Topics"
+                      className="bg-topics"
+                      value={topics}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="p-5 pb-1 mt-auto border-t-2 border-solid">
+              <button className="btn-wide">
+                {statusOutput(
+                  status,
+                  "View opportunity",
+                  "View project",
+                  "View project"
+                )}
+              </button>
             </div>
           </div>
-          <div className="p-5 pb-1 mt-auto border-t-2 border-solid">
-            <button className="btn-wide">
-              {statusOutput(
-                status,
-                "View opportunity",
-                "View project",
-                "View project"
-              )}
-            </button>
-          </div>
-        </div>
-      </Link>
-    </div>
+        </Link>
+      </div>
+    </article>
   )
 }
