5e45668e720a9afcd78b1895fc3c396096bb26b2
Josh <joshua_lu1@brown.edu>
2022-08-18 15:55:04 -0400

    refactor: make bottom banner link optional for pa11y

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -4,6 +4,7 @@ import React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { MarkdownText } from "../components"
 import language from "site/language.json"
+import { isNA } from "../utils"
 
 export const BottomBanner = () => {
   const { logo } = useStaticQuery(graphql`
@@ -22,15 +23,17 @@ export const BottomBanner = () => {
     <div className="py-6 mt-12 overflow-hidden bg-rd p-responsive">
       <div className="m-responsive">
         <div className="w-full flex gap-4 sm:gap-8 items-center justify-center flex-wrap">
-          <Link id="R+D link" to={language.bottom_banner.link}>
-            {image && (
-              <GatsbyImage
-                className="inline-block"
-                image={image}
-                alt="R+D logo"
-              />
-            )}
-          </Link>
+          {!isNA(language.bottom_banner.link) && (
+            <Link id="R+D link" to={language.bottom_banner.link}>
+              {image && (
+                <GatsbyImage
+                  className="inline-block"
+                  image={image}
+                  alt="R+D logo"
+                />
+              )}
+            </Link>
+          )}
           <div className="inline-block text-body flex-4 min-w-20ch">
             <MarkdownText text={language.bottom_banner.text} />
           </div>
