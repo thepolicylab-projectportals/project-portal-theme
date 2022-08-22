ec6ac1174b6a2fc9cc747916c065360e746fd004
Josh <joshua_lu1@brown.edu>
2022-08-22 13:42:25 -0400

    refactor: update bottom banner, allow for image but no link

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -23,16 +23,21 @@ export const BottomBanner = () => {
     <div className="py-6 mt-12 overflow-hidden bg-rd p-responsive">
       <div className="m-responsive">
         <div className="w-full flex gap-4 sm:gap-8 items-center justify-center flex-wrap">
-          {!isNA(language.bottom_banner.link) && (
+          {!isNA(language.bottom_banner.link) && image && (
             <Link id="R+D link" to={language.bottom_banner.link}>
-              {image && (
-                <GatsbyImage
-                  className="inline-block"
-                  image={image}
-                  alt="R+D logo"
-                />
-              )}
+              <GatsbyImage
+                className="inline-block"
+                image={image}
+                alt="R+D logo"
+              />
             </Link>
+          )}
+          {isNA(language.bottom_banner.link) && image && (
+            <GatsbyImage
+              className="inline-block"
+              image={image}
+              alt="R+D logo"
+            />
           )}
           <div className="inline-block text-body flex-4 min-w-20ch">
             <MarkdownText text={language.bottom_banner.text} />
