51704187b1679be3257c0f92756f3d245a9ca301
Josh <joshua_lu1@brown.edu>
2022-08-29 15:54:14 -0400

    refactor: move createPages to gatsby-node.js

--- project-portal-theme/gatsby-node.js
+++ project-portal-theme/gatsby-node.js
@@ -2,7 +2,62 @@
 
 require("source-map-support").install()
 
-const createPagesModule = require("./src/hooks/createPages")
+//const createPagesModule = require("./src/hooks/createPages")
 
-exports.createPages = createPagesModule.createPages
-exports.onCreatePage = createPagesModule.onCreatePage
+//exports.createPages = createPagesModule.createPages
+const tableName = "Project Page Content"
+const PartnerName = "Example Content"
+
+const path = require("path")
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  // Query for markdown nodes to use in creating pages.
+  const allAirtable = await graphql(`
+    {
+          allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+        }
+      }
+    }
+  }
+  `)
+
+  if (allAirtable.errors) {
+    reporter.panic("error loading projects", allAirtable.errors)
+    return
+  }
+
+  const projectDetailComponentPath = require.resolve(
+    "@thepolicylab-projectportals/project-portal-theme/src/templates/ProjectDetail.tsx"
+  )
+
+  allAirtable.data.allMarkdownRemark.nodes.forEach(({ frontmatter: { slug } }) => {
+    createPage({
+      path: `/project/${slug}`,
+      component: projectDetailComponentPath,
+      context: { slug },
+    })
+  })
+}
+
+exports.onCreatePage = ({ page, actions }, themeOptions) => {
+  const { createPage } = actions
+
+  createPage({
+    ...page,
+    context: {
+      ...page.context,
+      tableName,
+      partnerName: PartnerName,
+    },
+  })
+}
+
+console.log()
+
+
+//exports.onCreatePage = createPagesModule.onCreatePage
--- test_package.sh
+++ test_package.sh
@@ -15,9 +15,8 @@ echo "Initialize the new site"
 rsync -av --progress $siteDir/. $testDir --exclude node_modules --exclude .cache --exclude public
 
 echo "Package the theme"
-packName=$(cd $themeDir && npm pack)
+packName=$(cd $themeDir && yarn pack)
 echo "Theme name is: $packName"
 
 echo "Install the theme and then everything else"
-(cd $testDir && ln -s "node_modules/@thepolicylab-projectportals/project-portal-theme" themeDir) &
-(cd $testDir && npm install "../$themeDir/$packName" && npm install && npm run build && npm run serve --port=8111)
+(cd $testDir && yarn add "file:../$themeDir/$packName" && yarn install && yarn build && yarn serve --port=8111)
