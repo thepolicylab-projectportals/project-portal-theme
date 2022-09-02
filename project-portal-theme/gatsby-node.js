c214baab102089de204475361093a929a63f0c2f
John Gerrard Holland <john_holland1@brown.edu>
2022-09-02 14:10:02 -0400

    fix: convert createPages to pure JS

--- project-portal-theme/gatsby-node.js
+++ project-portal-theme/gatsby-node.js
@@ -2,10 +2,55 @@
 
 require("source-map-support").install()
 
-const createPagesModule = require("./src/hooks/createPages")
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  // Query for markdown nodes to use in creating pages.
+  const allAirtable = await graphql(`
+    {
+      allMarkdownRemark {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `)
 
-exports.createPages = createPagesModule.createPages
-exports.onCreatePage = createPagesModule.onCreatePage
+  if (allAirtable.errors) {
+    reporter.panic("error loading projects", allAirtable.errors)
+    return
+  }
+
+  const projectDetailComponentPath = require.resolve(
+    "@thepolicylab-projectportals/project-portal-theme/src/templates/ProjectDetail.tsx"
+  )
+
+  allAirtable.data.allMarkdownRemark.nodes.forEach(
+    ({ frontmatter: { slug } }) => {
+      createPage({
+        path: `/project/${slug}`,
+        component: projectDetailComponentPath,
+        context: { slug },
+      })
+    }
+  )
+}
+
+exports.onCreatePage = ({ page, actions }, themeOptions) => {
+  const { createPage } = actions
+
+  createPage({
+    ...page,
+    context: {
+      ...page.context,
+      tableName: themeOptions.tableName,
+      partnerName: themeOptions.PartnerName,
+    },
+  })
+}
 
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
