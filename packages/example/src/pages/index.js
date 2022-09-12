16dcf72576c0c53fd194f546b85fc7fe7974a8fb
John Gerrard Holland <john_holland1@brown.edu>
2022-09-12 17:00:23 -0400

    chore: remove redundant "home"

--- packages/example/src/pages/index.js
+++ packages/example/src/pages/index.js
@@ -1,15 +1,11 @@
 import React from "react"
-import Home from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components/Home"
-import BackIcon from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components/BackIcon"
-import ForwardIcon from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components/ForwardIcon"
+import { BackIcon, ForwardIcon } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
 
 const Index = () => {
 return (
   <>
-    <Home />
     <BackIcon />
     <ForwardIcon />
-
   </>
 );
 };
--- packages/gatsby-theme-project-portal/src/components/Home.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function Home() {
-    return <div><h1>Project Portal Theme</h1></div>
-}
