6dc0a9873bf4f804eb629bd7213e732fb90500b5
John Gerrard Holland <john_holland1@brown.edu>
2022-09-12 11:32:12 -0400

    add local packaging test

--- test-packaging-local.sh
+++ test-packaging-local.sh
@@ -6,8 +6,13 @@ die() {
   exit 1
 }
 
+artifactDir="$(pwd)/artifacts"
 testDir=$(mktemp -d)
 siteDir="packages/example/"
+themeName="@hollandjg/gatsby-theme-minimal"
+packPath="$artifactDir/theme-$(git rev-parse --short HEAD)-$(date '+%s').tgz"
+
+yarn workspace "$themeName" pack --out "$packPath"
 
 # Add the basic site content
 rsync -av --progress "$siteDir/." "$testDir" --exclude node_modules --exclude .cache --exclude public
@@ -16,7 +21,8 @@ rsync -av --progress "$siteDir/." "$testDir" --exclude node_modules --exclude .c
 cp ./packages/gatsby-theme-minimal/{.npmrc,.yarnrc.yml} "$testDir"
 
 (
-  cd "$testDir" || die "changing to testDir $testDir failed. Exiting."
-  yarn install
+  cd "$testDir" || die "changing to testDir $testDir failed."
+  yarn add $packPath || die "failed to install dependency $packPath."
+  yarn install || die "failed to install all dependencies"
   yarn build
 )
