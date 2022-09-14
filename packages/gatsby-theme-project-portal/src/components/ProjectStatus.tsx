3d1003ace4d64a520e2956332e87697b37263b0e
John Gerrard Holland <john_holland1@brown.edu>
2022-09-14 15:41:52 -0400

    chore: reformat code

--- packages/gatsby-theme-project-portal/src/components/ProjectStatus.tsx
+++ packages/gatsby-theme-project-portal/src/components/ProjectStatus.tsx
@@ -1,32 +1,34 @@
-import React, {FunctionComponent} from "react"
-import {FaCircle, FaPlayCircle, FaCheckCircle} from "react-icons/fa"
+import React, { FunctionComponent } from "react"
+import { FaCircle, FaPlayCircle, FaCheckCircle } from "react-icons/fa"
 
 interface ProjectStatusProps {
-    status: string
+  status: string
 }
 
 export const ProjectStatus: FunctionComponent<ProjectStatusProps> = ({
-                                                                         status,
-                                                                     }) => {
-    let Icon = null
-    let text: string = null
-    if (status == "open") {
-        Icon = FaCircle
-        text = "Open"
-    } else if (status == "ongoing") {
-        Icon = FaPlayCircle
-        text = "In progress"
-    } else if (status == "completed") {
-        Icon = FaCheckCircle
-        text = "Completed"
-    }
+  status,
+}) => {
+  let Icon = null
+  let text: string = null
+  if (status == "open") {
+    Icon = FaCircle
+    text = "Open"
+  } else if (status == "ongoing") {
+    Icon = FaPlayCircle
+    text = "In progress"
+  } else if (status == "completed") {
+    Icon = FaCheckCircle
+    text = "Completed"
+  }
 
-    return (<>
-        <div className="flex w-full">
+  return (
+    <>
+      <div className="flex w-full">
         <span className="pt-1 text-body text-primary align-inherit">
-          <Icon/>
+          <Icon />
         </span>
-            <span className="pl-2 uppercase">{text}</span>
-        </div>
-    </>)
+        <span className="pl-2 uppercase">{text}</span>
+      </div>
+    </>
+  )
 }
