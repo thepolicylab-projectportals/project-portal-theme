bb47c7f71d7be04bce28776d98b06d39c0234a21
John Gerrard Holland <john_holland1@brown.edu>
2022-09-14 14:44:54 -0400

    test: update test-scripts

--- test-packaging.sh
+++ test-packaging.sh
@@ -4,11 +4,12 @@
 die() {
   local message="$*"
   >&2 echo "${message}"
-  return 1
+  exit 1
 }
 
 # Specify the template site
 package-and-install () {
+
   # Specify how the package will be created
   # Options are
   # pack (default) – create and install a new pack-file,
@@ -34,10 +35,12 @@ package-and-install () {
   # Specify where the pack file is stored
   artifactDir="$(pwd)/artifacts"
 
+  publishTag="packageTest"
+
   # Specify if we serve the site at the end. Default: no.
   serve=0
 
-  while getopts st:n:a:m: flag
+  while getopts st:n:a:m:p: flag
   do
       case "${flag}" in
           t) {
@@ -48,80 +51,86 @@ package-and-install () {
           a) artifactDir=${OPTARG};;
           m) packageMethod=${OPTARG};;
           s) serve=1;;
+          p) publishTag=${OPTARG};;
           *) echo "flag ${flag} not recognized" ; return 1
       esac
   done
 
   echo "Running package-and-install with ${templateDir} ${themeName} ${artifactDir} ${packageMethod} ${initMethod} ${serve}"
 
-  case "${packageMethod}" in
-    pack) {
-      # Get the path where the pack-file will be created
-      packPath="$artifactDir/theme-$(date '+%s')-$(git rev-parse --short HEAD).tgz"
-      export packPath
-
-      # Create the pack file itself
-      yarn workspace "$themeName" pack --out "$packPath" || die "couldn't create pack-file directory"
-    };;
-    newest) {
-      # this doesn't do anything special – we just install as usual
-      echo "installing from remote"
-    };;
-    publish) {
-      # Publish the theme as a new version which will automatically be installed
-      yarn workspace "$themeName" version patch || die "If this fails, install 'yarn plugin import version' and rerun."
-
-      # Publish the theme
-      yarn workspace "$themeName" npm publish || die "If this fails, install 'yarn workspace $themeName npm login --publish' and rerun."
-    };;
-
-  esac
-
-  # Create an empty directory testDir where we can test the installation
-  testDir=$(mktemp -d || die "Failed to create new temporary directory.")
-  echo "new temporary directory: $testDir"
-
-  # Add files we need to ensure the installer looks in the right place for the package
-  cp ./packages/gatsby-theme-project-portal/{.npmrc,.yarnrc.yml} "$testDir" || die "couldn't copy rc-files"
-
-  case "${initMethod}" in
-    empty) {
-      (
-        cd $testDir || die "Failed to cd to testDir '$testDir'"
-        yarn init -y || die "Failed to init new site"
-        case "${packageMethod}" in
-          pack) yarn add "$packPath" react react-dom gatsby || die "failed to install dependencies including $packPath.";;
-          *) yarn add react react-dom gatsby "$themeName" || die "Failed to add dependencies";
-        esac
-        echo "module.exports = { plugins: [\`@thepolicylab-projectportals/gatsby-theme-project-portal\`] }" > "$testDir/gatsby-config.js"
-      )
-    };;
-    template) {
-      # Sync content from the template site to the testDir
-      rsync -av --progress "$templateDir/." "$testDir" --exclude node_modules --exclude .cache --exclude public
-    };;
-  esac
-
-
-  # Navigate to the test directory
+  # Spawn a new subshell to carry out the rest of the commands.
+  # This way, if anything "dies", it only kills the subshell, and not the whole shell session.
   (
-    cd $testDir || die "couldn't cd to testDir '$testDir'"
+    case "${packageMethod}" in
+      pack) {
+        # Get the path where the pack-file will be created
+        packPath="$artifactDir/theme-$(date '+%s')-$(git rev-parse --short HEAD).tgz"
+        export packPath
 
-    # Install the rest of the dependencies
-    yarn install || die "failed to install all dependencies"
+        # Create the pack file itself
+        yarn workspace "$themeName" pack --out "$packPath" || die "couldn't create pack-file directory"
+      };;
+      newest) {
+        # this doesn't do anything special – we just install as usual
+        echo "installing from remote"
+      };;
+      publish) {
+        # Publish the theme as a new version which will automatically be installed
+         yarn workspace "$themeName" version patch || die "If this fails, install 'yarn plugin import version' and rerun."
 
-    # Build the site
-    yarn gatsby build || die "failed to build site"
+        # Publish the theme
+        yarn workspace "$themeName" npm publish --tag "$publishTag" || die "If this fails, install 'yarn workspace $themeName npm login --publish' and rerun."
+      };;
 
-    # Serve the site
-    if [[ ${serve} -eq 1 ]]
-    then
-      yarn gatsby serve
-    fi
+    esac
+
+    # Create an empty directory testDir where we can test the installation
+    testDir=$(mktemp -d || die "Failed to create new temporary directory.")
+    echo "new temporary directory: $testDir"
+
+    # Add files we need to ensure the installer looks in the right place for the package
+    cp ./packages/gatsby-theme-project-portal/{.npmrc,.yarnrc.yml} "$testDir" || die "couldn't copy rc-files"
+
+    case "${initMethod}" in
+      empty) {
+        (
+          cd $testDir || die "Failed to cd to testDir '$testDir'"
+          yarn init -y || die "Failed to init new site"
+          case "${packageMethod}" in
+            pack) yarn add "$packPath" react react-dom gatsby || die "failed to install dependencies including $packPath.";;
+            *) yarn add react react-dom gatsby "$themeName@$publishTag" || die "Failed to add dependencies";
+          esac
+          echo "module.exports = { plugins: [\`@thepolicylab-projectportals/gatsby-theme-project-portal\`] }" > "$testDir/gatsby-config.js"
+        )
+      };;
+      template) {
+        # Sync content from the template site to the testDir
+        rsync -av --progress "$templateDir/." "$testDir" --exclude node_modules --exclude .cache --exclude public
+      };;
+    esac
+
+
+    # Navigate to the test directory
+    (
+      cd $testDir || die "couldn't cd to testDir '$testDir'"
+
+      # Install the rest of the dependencies
+      yarn install || die "failed to install all dependencies"
+
+      # Build the site
+      yarn gatsby build || die "failed to build site"
+
+      echo ""
+      echo "Site built successfully. To serve run:"
+      echo "(cd $testDir && yarn gatsby serve)"
+
+      # Serve the site
+      if [[ ${serve} -eq 1 ]]
+      then
+        yarn gatsby serve
+      fi
+    )
   )
-  echo ""
-  echo "Site built successfully. To serve run"
-  echo "(cd $testDir && yarn gatsby serve)"
 }
 
 # Define some standard cases as aliases.
@@ -134,14 +143,16 @@ alias test-pack-template-example="package-and-install -m pack -t packages/exampl
 alias test-newest-empty="package-and-install -m newest"
 alias test-newest-template-defaults="package-and-install -m newest -t packages/defaults/"
 alias test-newest-template-example="package-and-install -m newest -t packages/example/"
-alias test-publish-template-example="package-and-install -m publish -t packages/example/"
 
-run-all-packaging-tests () {
-  test-pack-empty  || { echo "packaging failed: 'test-pack-empty'" ; return 1; }
-  test-pack-template-defaults || { echo "packaging failed: 'test-pack-template-defaults'" ; return 1; }
-  test-pack-template-example  || { echo "packaging failed: 'test-pack-template-example'" ; return 1; }
-  test-publish-template-example  || { echo "packaging failed: 'test-publish-template-example'" ; return 1; }
-  test-newest-empty  || { echo "packaging failed: 'test-newest-empty'" ; return 1; }
-  test-newest-template-defaults  || { echo "packaging failed: 'test-newest-template-defaults'" ; return 1; }
-  test-newest-template-example  || { echo "packaging failed: 'test-newest-template-example'" ; return 1; }
+run-all-local-packaging-tests () {
+  (
+    test-pack-empty || die "packaging failed: 'test-pack-empty'"
+    test-pack-template-defaults || die "packaging failed: 'test-pack-template-defaults'"
+    test-pack-template-example  || die "packaging failed: 'test-pack-template-example'"
+    test-newest-empty  || die "packaging failed: 'test-newest-empty'"
+    test-newest-template-defaults  || die "packaging failed: 'test-newest-template-defaults'"
+    test-newest-template-example  || die "packaging failed: 'test-newest-template-example'"
+  )
 }
+
+alias test-publish-template-example="package-and-install -m publish -t packages/example/"
