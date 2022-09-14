dfab1f6cfe0eb532158482749c7b976e1bd2b66d
John Gerrard Holland <john_holland1@brown.edu>
2022-09-14 16:14:04 -0400

    chore: run prettier code formatter

--- packages/example/src/pages/index.js
+++ packages/example/src/pages/index.js
@@ -1,16 +1,16 @@
 import React from "react"
-import { BackIcon, ForwardIcon } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
+import {
+  BackIcon,
+  ForwardIcon,
+} from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
 
 const Index = () => {
-return (
-  <>
-    <BackIcon />
-    <ForwardIcon />
-  </>
-);
-};
+  return (
+    <>
+      <BackIcon />
+      <ForwardIcon />
+    </>
+  )
+}
 
-export default Index;
-
-
-
+export default Index
