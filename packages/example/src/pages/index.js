b624b62902b99909f125dd93a0ab742d1c73d880
John Gerrard Holland <john_holland1@brown.edu>
2022-09-16 11:04:00 -0400

    feat: add example MarkdownText to index

--- packages/example/src/pages/index.js
+++ packages/example/src/pages/index.js
@@ -4,7 +4,22 @@ import {
   ForwardIcon,
   DevelopmentBanner,
   ProjectStatus,
+  MarkdownText,
 } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
+
+var markdownContent = `
+# Example Markdown Content
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit ac nunc et aliquet. 
+
+## Heading 2
+
+In tristique dolor porttitor magna tincidunt blandit. Suspendisse vel orci ut nisi molestie finibus sed sodales elit. 
+
+### Heading 3
+
+Maecenas eget facilisis risus. Nunc dignissim ante nec mauris sollicitudin, ac aliquam turpis ullamcorper. 
+`
 
 const Index = () => {
   return (
@@ -15,6 +30,7 @@ const Index = () => {
       <ProjectStatus status="open" />
       <ProjectStatus status="ongoing" />
       <ProjectStatus status="completed" />
+      <MarkdownText text={markdownContent} />
     </>
   )
 }
