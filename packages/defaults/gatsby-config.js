cfebb29dff3b3423ee270fa21eaa28b61e992a41
aomar7 <aisuluomar123@gmail.com>
2022-09-29 16:14:08 -0400

    Fix: updating most recent packages in default

--- packages/defaults/gatsby-config.js
+++ packages/defaults/gatsby-config.js
@@ -1,3 +1,26 @@
+const staticText = require("./language.json")
+
+// General metadata for the site
+const siteMetadata = {
+  siteTitle: "Default Project Portal Content",
+  short_name: "Project Portal",
+  siteUrl: process.env.CI ? "http://localhost:9000" : "https://localhost",
+  projectInterestLink: "",
+  live: false,
+  locale: "en",
+}
+
 module.exports = {
-  plugins: [`@thepolicylab-projectportals/gatsby-theme-project-portal`],
+  siteMetadata: siteMetadata,
+
+  plugins: [
+    {
+      resolve: `@thepolicylab-projectportals/gatsby-theme-project-portal`,
+      options: {
+        staticText: staticText,
+        contentPath: `${__dirname}/content`,
+        imagePath: `${__dirname}/content/images`,
+      },
+    },
+  ],
 }
