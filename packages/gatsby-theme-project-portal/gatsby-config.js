d6455cbb8fad79564b7500f6c24debf973884db3
aomar7 <aisuluomar123@gmail.com>
2022-09-30 15:27:24 -0400

    Feat: Adding Bottom Banner component and plugins

--- packages/gatsby-theme-project-portal/gatsby-config.js
+++ packages/gatsby-theme-project-portal/gatsby-config.js
@@ -1,1 +1,14 @@
-module.exports = {}
+module.exports = {
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+}
