4cfa7b4ea7984a0cbfff627753b9c34be6fcc451
John Gerrard Holland <john_holland1@brown.edu>
2022-09-30 16:29:10 -0400

    feat: add CollaboratorDetails component

--- packages/example/src/pages/index.js
+++ packages/example/src/pages/index.js
@@ -10,6 +10,7 @@ import {
   SectionOfItem,
   Card,
   Cards,
+  CollaboratorDetails,
 } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
 
 const markdownContent = `
@@ -67,6 +68,13 @@ const sample_cards = [
   },
 ]
 
+const collaborator_details = {
+  expertise: "- Collaborator.\n- Details.\n- Expertise.\n",
+  requirement: "Must be a collaborator\n",
+  keyDates:
+    "We are ready to begin the project as soon as we identify a collaborator.\n",
+}
+
 const Index = () => {
   return (
     <>
@@ -82,6 +90,14 @@ const Index = () => {
       <SectionOfItem label="Section of Items" value={markdownContent} />
       <Card {...sample_card} />
       <Cards nodes={sample_cards} />
+      {/*Normal Case for Collaborator Details*/}
+      <CollaboratorDetails {...collaborator_details} />
+      {/*No Collaborator Details*/}
+      <CollaboratorDetails />
+      {/*Minimal data – one field only*/}
+      <CollaboratorDetails expertise={"Expertise only"} />
+      <CollaboratorDetails requirement={"Requirement only"} />
+      <CollaboratorDetails keyDates={"Key dates only"} />
     </>
   )
 }
