a13690c9cd2cdb20c57d512824f76e8b2a96eeb9
aomar7 <aisuluomar123@gmail.com>
2022-10-01 20:25:28 -0400

    Feat: Adding packages

--- packages/gatsby-theme-project-portal/gatsby-config.js
+++ packages/gatsby-theme-project-portal/gatsby-config.js
@@ -1,14 +1,1 @@
-module.exports = {
-  plugins: [
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-  ],
-}
+module.exports = {}
