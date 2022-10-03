7decc88712e276a7769076bf5ddfe2ba77eba903
aomar7 <aisuluomar123@gmail.com>
2022-10-03 17:11:01 -0400

    Fix: renaming BottomBanner queries

--- packages/example/src/pages/index.js
+++ packages/example/src/pages/index.js
@@ -11,7 +11,7 @@ import {
   Card,
   Cards,
   NavbarLayout,
-  BottomBanner,
+  BottomBannerLayout,
 } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
 
 import { useStaticQuery, graphql } from "gatsby"
@@ -99,15 +99,16 @@ const Index = () => {
           gatsbyImageData(width: 64)
         }
       }
-      bannerLogo: file(relativePath: { regex: "/^rd_logo.png$/" }) {
+      BottomBanner: file(relativePath: { regex: "/^rd_logo.png$/" }) {
         childImageSharp {
           gatsbyImageData(width: 160)
         }
       }
     }
   `)
+  const link = "https://www.nc.gov/terms"
   const navbarLogoImage = getImage(query.logo)
-  const bannerLogoImage = getImage(query.bannerLogo)
+  const bannerLogoImage = getImage(query.BottomBanner)
   const nav_image = (
     <GatsbyImage
       className="hidden xl:inline-block"
@@ -140,7 +141,8 @@ const Index = () => {
         pages={pages}
         activePage="First Nav"
       />
-      <BottomBanner image={banner_image} text="Sample text" />
+      <BottomBannerLayout image={banner_image} text="Sample text" link={link} />
+      <BottomBannerLayout image={banner_image} text="Sample text" />
       <BackIcon />
       <ForwardIcon />
       <ProjectStatus status="open" />
