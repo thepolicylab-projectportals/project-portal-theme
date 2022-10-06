7fd6bf2c7a223a7f159f66137677dfa5033e213a
aomar7 <aisuluomar123@gmail.com>
2022-10-06 09:13:47 -0400

    Fix: Adding sitemetadata to defaults

--- packages/defaults/gatsby-config.js
+++ packages/defaults/gatsby-config.js
@@ -1,3 +1,13 @@
+const siteMetadata = {
+  siteTitle: "Gatsby Theme Project Portal",
+  short_name: "Project Portal",
+  siteUrl: process.env.CI ? "http://localhost:9000" : "https://localhost",
+  projectInterestLink: "",
+  live: false,
+  locale: "en",
+  pages: [{ name: "Home", link: "/", show: true }],
+}
 module.exports = {
+  siteMetadata: siteMetadata,
   plugins: [`@thepolicylab-projectportals/gatsby-theme-project-portal`],
 }
