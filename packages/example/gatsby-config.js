9a286085c8aeb33ca00cb88274ec01a53182e4f0
Aisulu Omar <41978201+AisOmar@users.noreply.github.com>
2022-10-06 08:06:07 -0400

    Update packages/example/gatsby-config.js
    Co-authored-by: John Gerrard Holland <john_holland1@brown.edu>

--- packages/example/gatsby-config.js
+++ packages/example/gatsby-config.js
@@ -3,7 +3,6 @@ const siteMetadata = {
   siteTitle: "Gatsby Theme Project Portal",
   short_name: "Project Portal",
   siteUrl: process.env.CI ? "http://localhost:9000" : "https://localhost",
-  projectInterestLink: "",
   live: false,
   locale: "en",
   pages: [{ name: "Home", link: "/", show: true }],
