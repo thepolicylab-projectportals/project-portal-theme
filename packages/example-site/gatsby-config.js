10cf90232ffbcca9d2f97444de7f5704afaa421b
John Gerrard Holland <john_holland1@brown.edu>
2022-10-12 14:13:09 -0400

    feat: add new example site with BottomBanner component included in index

---
+++ packages/example-site/gatsby-config.js
@@ -0,0 +1,7 @@
+// General metadata for the site
+module.exports = {
+  siteMetadata: {
+    title: "Project Portal Example (Site with Queries)",
+  },
+  plugins: [`@thepolicylab-projectportals/gatsby-theme-project-portal`],
+}
