8cc1b8d6248e24cd03cb0fc7efa435f15d9d2a6f
John Gerrard Holland <john_holland1@brown.edu>
2022-10-12 16:19:54 -0400

    feat: add types (all except for "MainContact" and "ProjectTeam") to Project schema

--- packages/gatsby-theme-project-portal-netlify-content/gatsby-node.js
+++ packages/gatsby-theme-project-portal-netlify-content/gatsby-node.js
@@ -3,10 +3,34 @@ const PROJECT_NODE_TYPE = `Project`
 const projectTypeDefs = `
     type ${PROJECT_NODE_TYPE} implements Node {
       slug: String!
-      description: String
+      
+      question: String
+      partnerName: String
+      status: String
+      
+      opportunityCloses: Date @dateformat
+      startDate: Date @dateformat
+      endDate: Date @dateformat
+  
+      agency: String
+      topics: [String]
+      supportNeeded: [String]
+      
+      summary: String
+      deliverable: String
+      purpose: String
+      expertise: String
+      requirement: String
+      keyDates: String
+      statusOfData: String
+      priorResearch: String
+      fundingInfo: String
+      emailContent: String
+      
+      lastModified: Date @dateformat(formatString: "YYYY-MM-DDTHH:mm:ss.SSSZ")
+      
     }
   `
-
 exports.sourceNodes = async ({
   actions,
   createContentDigest,
@@ -20,6 +44,11 @@ exports.sourceNodes = async ({
       { slug: "hello-world-project", description: `Hello world!` },
       { slug: "second-project", description: `Second project!` },
       { slug: "third-project" },
+      {
+        slug: "fourth-project",
+        lastModified: "2022-10-12",
+        startDate: "2022-10-13",
+      },
     ],
   }
 
