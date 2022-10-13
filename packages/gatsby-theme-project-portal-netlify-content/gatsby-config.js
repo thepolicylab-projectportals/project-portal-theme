93065365a17afccce3cb7f03e759b4dfc22fd030
John Gerrard Holland <john_holland1@brown.edu>
2022-10-13 08:32:34 -0400

    feat: add gatsby-source-filesystem for project files

--- packages/gatsby-theme-project-portal-netlify-content/gatsby-config.js
+++ packages/gatsby-theme-project-portal-netlify-content/gatsby-config.js
@@ -1,1 +1,15 @@
-module.exports = {}
+const { withDefaults } = require("./utils/default-options")
+module.exports = (pluginOptions) => {
+  pluginOptionsWithDefaults = withDefaults(pluginOptions)
+  return {
+    plugins: [
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `project`,
+          path: pluginOptionsWithDefaults.projectPath,
+        },
+      },
+    ],
+  }
+}
