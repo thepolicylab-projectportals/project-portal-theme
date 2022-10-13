5c32e53b44a5f42c538b930d1ce1e4d0f2c04b03
John Gerrard Holland <john_holland1@brown.edu>
2022-10-13 08:24:46 -0400

    feat: add automated creation of projectPath within the site directory

--- packages/gatsby-theme-project-portal-netlify-content/gatsby-node.js
+++ packages/gatsby-theme-project-portal-netlify-content/gatsby-node.js
@@ -1,6 +1,19 @@
+const fs = require("fs")
+
 const {
   PROJECT_NODE_TYPE,
 } = require("@thepolicylab-projectportals/gatsby-theme-project-portal/utils/types")
+const { withDefaults } = require("./utils/default-options")
+
+exports.onPreBootstrap = ({ reporter }, pluginOptions) => {
+  console.log(withDefaults(pluginOptions))
+  const { projectPath } = withDefaults(pluginOptions)
+
+  if (!fs.existsSync(projectPath)) {
+    reporter.info(`creating the ${projectPath} directory`)
+    fs.mkdirSync(projectPath, { recursive: true })
+  }
+}
 
 exports.sourceNodes = async ({
   actions,
