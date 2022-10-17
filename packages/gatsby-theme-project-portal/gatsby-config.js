ed305b42e0b58039bbfd82dfd6a96a36e88d83f4
aomar7 <aisuluomar123@gmail.com>
2022-10-17 12:00:58 -0400

    Feat: adding taiwind purge

--- packages/gatsby-theme-project-portal/gatsby-config.js
+++ packages/gatsby-theme-project-portal/gatsby-config.js
@@ -1,17 +1,29 @@
 const { withDefaults } = require(`./utils/default-options`)
 
 module.exports = (themeOptions) => {
-  console.log("theme options: ", themeOptions)
-  console.log("theme options with defaults: ", withDefaults(themeOptions))
+  // console.log("theme options: ", themeOptions)
+  // console.log("theme options with defaults: ", withDefaults(themeOptions))
+  const siteMetadata = {
+    title: "Gatsby Theme Project Portal",
+    description:
+      "The Project Portal, developed by the Policy Lab at Brown University.",
+    url: `http://localhost:${process.env.CI ? 9000 : process.env.PORT ?? ``}`,
+    locale: "en",
+    image: "/icons/icon-256x256.png",
+  }
 
   return {
-    siteMetadata: {
-      title: "Gatsby Theme Project Portal",
-      description:
-        "The Project Portal, developed by the Policy Lab at Brown University.",
-      url: `http://localhost:${process.env.CI ? 9000 : process.env.PORT ?? ``}`,
-      locale: "en",
-      image: "/icons/icon-256x256.png",
-    },
+    siteMetadata: siteMetadata,
+    plugins: [
+      {
+        resolve: `gatsby-plugin-postcss`,
+        options: {
+          postCssPlugins: [
+            require("tailwindcss")(themeOptions.tailwindConfig),
+            require("autoprefixer"),
+          ],
+        },
+      },
+    ],
   }
 }
