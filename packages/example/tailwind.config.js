1a29ea01356639a2ab9ee222e2f4a21191db00d7
John Gerrard Holland <john_holland1@brown.edu>
2022-10-18 10:34:53 -0400

    feat: add automatic finding of the theme directory, wherever it is.

--- packages/example/tailwind.config.js
+++ packages/example/tailwind.config.js
@@ -1,3 +1,17 @@
+const path = require("path")
+
+// Get the `main` file listed in the theme's package.json.
+// This should (hopefully) find the right directory with node_modules,
+// as well as with the yarn berry unpacking approach.
+const themeMainFile = require.resolve(
+  `@thepolicylab-projectportals/gatsby-theme-project-portal`
+)
+console.log("theme main file: ", themeMainFile)
+
+// Get the path to the theme's directory, without a trailing slash
+const themeDirectory = path.dirname(themeMainFile)
+console.log("theme directory: ", themeDirectory)
+
 module.exports = {
   presets: [
     require("@thepolicylab-projectportals/gatsby-theme-project-portal/src/styles/tailwind.presets"),
@@ -5,7 +19,9 @@ module.exports = {
   purge: {
     enabled: true,
     content: [
-      "./node_modules/@thepolicylab-projectportals/gatsby-theme-project-portal/src/**/*.{js,jsx,ts,tsx,html}",
+      // The theme components
+      themeDirectory + "/src/**/*.{js,jsx,ts,tsx,html}",
+      // The local src components
       "./src/**/*.{js,jsx,ts,tsx,html}",
     ],
   },
