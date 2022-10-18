1a4f9021e119a43d1463a0ef7dc87909570d5057
aomar7 <aisuluomar123@gmail.com>
2022-10-18 11:45:11 -0400

    Feat: adding tailwind to example site

--- packages/example-site/gatsby-config.js
+++ packages/example-site/gatsby-config.js
@@ -1,12 +1,17 @@
 // General metadata for the site
+const tailwindConfig = require("./tailwind.config")
+const siteMetadata = {
+  title: "Project Portal Example (Site with Queries)",
+  short_name: "Project Portal Site",
+}
+
 module.exports = {
-  siteMetadata: {
-    title: "Project Portal Example (Site with Queries)",
-  },
+  siteMetadata: siteMetadata,
   plugins: [
     {
       resolve: `@thepolicylab-projectportals/gatsby-theme-project-portal`,
       options: {
+        tailwindConfig: tailwindConfig,
         staticText: {
           bottom_banner: {
             text: "This is the Project Portal site footer text.",
