fad5bbef5871dd6f2f4f7b4ad63748bc3a3c0b56
aomar7 <aisuluomar123@gmail.com>
2022-10-20 17:45:25 -0400

    Feat: adding index page to example-content

--- packages/example-content/gatsby-config.js
+++ packages/example-content/gatsby-config.js
@@ -1,6 +1,28 @@
+const tailwindConfig = require("example-site/tailwind.config")
+
 module.exports = {
+  siteMetadata: {
+    title: "Project Portal Example (Test Data)",
+  },
   plugins: [
-    `@thepolicylab-projectportals/gatsby-theme-project-portal`,
     `@thepolicylab-projectportals/project-portal-content-netlify`,
+    {
+      resolve: `@thepolicylab-projectportals/gatsby-theme-project-portal`,
+      options: {
+        pages: [
+          {
+            name: "Landing Page",
+            link: "/",
+            show: true,
+          },
+        ],
+        tailwindConfig: tailwindConfig,
+        staticText: {
+          bottom_banner: {
+            text: "This is the Project Portal site footer text.",
+          },
+        },
+      },
+    },
   ],
 }
