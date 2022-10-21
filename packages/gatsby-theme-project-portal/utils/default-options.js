f5709d2061bf9768b8e6a8c126f61b28674f6b5c
John Gerrard Holland <john_holland1@brown.edu>
2022-10-21 19:39:55 -0400

    test:remove duplicate fields in withDefaults

--- packages/gatsby-theme-project-portal/utils/default-options.js
+++ packages/gatsby-theme-project-portal/utils/default-options.js
@@ -9,10 +9,6 @@ function withDefaults(themeOptions) {
     themeImageDirectory:
       themeOptions.themeImageDirectory || "./content/theme-image",
     siteTitle: themeOptions.siteTitle || `Gatsby Theme Project Portal`,
-    showDevBanner: themeOptions.showDevBanner || true,
-    staticText: lodash.merge(defaultStaticText, themeOptions.staticText),
-    themeImageDirectory:
-      themeOptions.themeImageDirectory || "./content/theme-image",
     pages: themeOptions.pages || [
       {
         name: "Home",
