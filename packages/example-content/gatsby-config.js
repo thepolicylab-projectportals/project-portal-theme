d59b987e23751c685e409e6450fb4fc465a0cf97
aomar7 <aisuluomar123@gmail.com>
2022-10-21 09:29:17 -0400

    Feat: additional components for netlify cms

--- packages/example-content/gatsby-config.js
+++ packages/example-content/gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
             link: "/",
             show: true,
           },
+          { name: "Add Projects", link: "/admin/", show: true },
         ],
         tailwindConfig: tailwindConfig,
         staticText: {
--- packages/project-portal-content-netlify/gatsby-config.js
+++ packages/project-portal-content-netlify/gatsby-config.js
@@ -1,9 +1,18 @@
 const { withDefaults } = require("./utils/default-options")
+const tailwindConfig = require("example-site/tailwind.config")
 module.exports = (pluginOptions) => {
   const pluginOptionsWithDefaults = withDefaults(pluginOptions)
   return {
     plugins: [
       `gatsby-transformer-json`,
+      {
+        resolve: `gatsby-plugin-netlify-cms`,
+        options: {
+          // modulePath: tailwindConfig,
+          enableIdentityWidget: true,
+          publicPath: `admin`,
+        },
+      },
       {
         resolve: `gatsby-source-filesystem`,
         options: {
