1737133cee11f90b36dc8ffda92cbb2eedfeeff6
John Gerrard Holland <john_holland1@brown.edu>
2022-10-24 12:13:46 -0400

    chore: remove extra files in example-content

--- packages/example-content/gatsby-config.js
+++ packages/example-content/gatsby-config.js
@@ -1,28 +1,13 @@
-const tailwindConfig = require("example-content/tailwind.config")
-
 module.exports = {
-  siteMetadata: {
-    title: "Project Portal Example (Test Data)",
-  },
   plugins: [
     `@thepolicylab-projectportals/project-portal-content-netlify`,
     {
       resolve: `@thepolicylab-projectportals/gatsby-theme-project-portal`,
       options: {
         pages: [
-          {
-            name: "Landing Page",
-            link: "/",
-            show: true,
-          },
+          { name: "Home", link: "/", show: true },
           { name: "Add Projects", link: "/admin/", show: true },
         ],
-        tailwindConfig: tailwindConfig,
-        staticText: {
-          bottom_banner: {
-            text: "This is the Project Portal site footer text.",
-          },
-        },
       },
     },
   ],
