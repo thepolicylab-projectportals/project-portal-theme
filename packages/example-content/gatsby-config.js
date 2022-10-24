240d724fc76db238b6d6a797635be5e6b7a0c554
John Gerrard Holland <john_holland1@brown.edu>
2022-10-24 19:12:56 -0400

    refactor: reorder imports in example-content

--- packages/example-content/gatsby-config.js
+++ packages/example-content/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    `@thepolicylab-projectportals/project-portal-content-netlify`,
     `@thepolicylab-projectportals/gatsby-theme-project-portal`,
+    `@thepolicylab-projectportals/project-portal-content-netlify`,
   ],
 }
