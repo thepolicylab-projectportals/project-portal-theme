af1ed94aa19335c1fda7747eb6296c4834d036df
John Gerrard Holland <john_holland1@brown.edu>
2022-10-24 20:41:11 -0400

    feat: split creation of airtable projects into two steps (so we can restructure on the way)

--- packages/project-portal-content-airtable/gatsby-node.js
+++ packages/project-portal-content-airtable/gatsby-node.js
@@ -9,30 +9,11 @@ const { withDefaults } = require("./utils/default-options")
 // If in doubt, check the GraphQL and look at the internal.type field of the
 // nodes created by the gatsby-transformer-json. Modify these parameters
 // to match.
+const RAW_PROJECT_AIRTABLE_TYPE = `AirtableRawProject`
+const RAW_CONTACT_AIRTABLE_TYPE = `AirtableRawContact`
+// These are wanted output types
 const PROJECT_AIRTABLE_TYPE = `AirtableProject`
 const CONTACT_AIRTABLE_TYPE = `AirtableContact`
-
-exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions
-  // We specify the date-types for the ProjectJson
-  // to ensure we load the correct format for the
-  // Project nodes
-  const allAirtableTypeDefs = `
-    type ${PROJECT_AIRTABLE_TYPE} implements Node {
-      data: ProjectAirtableData
-    }
-    type ProjectAirtableData {
-      slug: String
-      showMainContactOnProjectTeam: Boolean
-      opportunityCloses: Date @dateformat(formatString: "YYYY-MM-DD")
-      startDate: Date @dateformat(formatString: "YYYY-MM-DD")
-      endDate: Date @dateformat(formatString: "YYYY-MM-DD")
-      lastModified: Date @dateformat(formatString: "YYYY-MM-DDTHH:mm:ss.SSSZ")
-      contacts: [String]
-      }
-  `
-  createTypes(allAirtableTypeDefs)
-}
 
 exports.onCreateNode = async (
   {
@@ -47,7 +28,7 @@ exports.onCreateNode = async (
     withDefaults(pluginOptions)
 
   if (
-    node.internal.type === PROJECT_AIRTABLE_TYPE &&
+    node.internal.type === RAW_PROJECT_AIRTABLE_TYPE &&
     node.data.partnerName === partnerName
   ) {
     const project = node
@@ -99,16 +80,16 @@ exports.onCreateNode = async (
     }
     createNode({
       ...projectRestructured,
-      id: createNodeId(`${PROJECT_NODE_TYPE}-${projectRestructured.slug}`),
+      id: createNodeId(`${PROJECT_AIRTABLE_TYPE}-${projectRestructured.slug}`),
       parent: null,
       children: [],
       internal: {
-        type: `${PROJECT_NODE_TYPE}`,
+        type: `${PROJECT_AIRTABLE_TYPE}`,
         contentDigest: createContentDigest(projectRestructured),
       },
     })
   }
-  if (node.internal.type === CONTACT_AIRTABLE_TYPE) {
+  if (node.internal.type === RAW_CONTACT_AIRTABLE_TYPE) {
     const contact = node
     const contactRestructured = {
       key: contact.recordId,
@@ -122,13 +103,145 @@ exports.onCreateNode = async (
 
     createNode({
       ...contactRestructured,
-      id: createNodeId(`${CONTACT_NODE_TYPE}-${contactRestructured.key}`),
+      id: createNodeId(`${CONTACT_AIRTABLE_TYPE}-${contactRestructured.key}`),
       parent: null,
       children: [],
       internal: {
-        type: `${CONTACT_NODE_TYPE}`,
+        type: `${CONTACT_AIRTABLE_TYPE}`,
         contentDigest: createContentDigest(contactRestructured),
       },
     })
   }
 }
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  const { createTypes } = actions
+
+  // const typeDefs = [
+  //   schema.buildObjectType({
+  //     name: PROJECT_AIRTABLE_TYPE,
+  //     interfaces: ["Node", PROJECT_NODE_TYPE],
+  //     fields: {
+  //       slug: "String!",
+  //       question: "String",
+  //       status: "String",
+  //       opportunityCloses: {
+  //         type: "Date",
+  //         extensions: {
+  //           dateformat: {},
+  //         },
+  //       },
+  //       startDate: {
+  //         type: "Date",
+  //         extensions: {
+  //           dateformat: {},
+  //         },
+  //       },
+  //       endDate: {
+  //         type: "Date",
+  //         extensions: {
+  //           dateformat: {},
+  //         },
+  //       },
+  //
+  //       agency: "String",
+  //       topics: "[String]",
+  //
+  //       summary: "String",
+  //       deliverable: "String",
+  //       purpose: "String",
+  //       expertise: "String",
+  //       requirement: "String",
+  //       keyDates: "String",
+  //       statusOfData: "String",
+  //       priorResearch: "String",
+  //       fundingInfo: "String",
+  //       emailContent: "String",
+  //
+  //       lastModified: {
+  //         type: "Date",
+  //         extensions: {
+  //           dateformat: { formatString: "YYYY-MM-DDTHH:mm:ss.SSSZ" },
+  //         },
+  //       },
+  //
+  //       mainContact: {
+  //         type: CONTACT_NODE_TYPE,
+  //         resolve: async (source, args, context) => {
+  //           return await context.nodeModel.findOne({
+  //             type: CONTACT_AIRTABLE_TYPE,
+  //             query: {
+  //               filter: { key: { eq: source.mainContact } },
+  //             },
+  //           })
+  //         },
+  //       },
+  //       projectTeam: {
+  //         type: [CONTACT_AIRTABLE_TYPE],
+  //         resolve: async (source, args, context) => {
+  //           const { entries } = await context.nodeModel.findAll({
+  //             type: CONTACT_AIRTABLE_TYPE,
+  //             query: {
+  //               filter: { key: { in: source.projectTeam ?? [] } },
+  //             },
+  //           })
+  //           return entries
+  //         },
+  //       },
+  //     },
+  //   }),
+  //   schema.buildObjectType({
+  //     name: CONTACT_AIRTABLE_TYPE,
+  //     interfaces: ["Node", CONTACT_NODE_TYPE],
+  //     fields: {
+  //       key: "String!",
+  //       email: "String",
+  //       name: "String",
+  //       employer: "String",
+  //       title: "String",
+  //       image: {
+  //         type: "File",
+  //       },
+  //       // image: {
+  //       //   type: "File",
+  //       //   resolve: async (source, args, context) => {
+  //       //     return await context.nodeModel.findOne({
+  //       //       type: "File",
+  //       //       query: {
+  //       //         filter: {
+  //       //           relativePath: { eq: source.image },
+  //       //           sourceInstanceName: { eq: "contact" },
+  //       //         },
+  //       //       },
+  //       //     })
+  //       //   },
+  //       // },
+  //     },
+  //   }),
+  // ]
+  // createTypes(typeDefs)
+}
+//
+//
+// exports.createSchemaCustomization = ({ actions }) => {
+//   const { createTypes } = actions
+//   // We specify the date-types for the ProjectJson
+//   // to ensure we load the correct format for the
+//   // Project nodes
+//   const allAirtableTypeDefs = `
+//     type ${PROJECT_AIRTABLE_TYPE} implements Node {
+//       data: ProjectAirtableData
+//     }
+//     type ProjectAirtableData {
+//       slug: String
+//       showMainContactOnProjectTeam: Boolean
+//       opportunityCloses: Date @dateformat(formatString: "YYYY-MM-DD")
+//       startDate: Date @dateformat(formatString: "YYYY-MM-DD")
+//       endDate: Date @dateformat(formatString: "YYYY-MM-DD")
+//       lastModified: Date @dateformat(formatString: "YYYY-MM-DDTHH:mm:ss.SSSZ")
+//       contacts: [String]
+//       }
+//   `
+//   createTypes(allAirtableTypeDefs)
+// }
+//
