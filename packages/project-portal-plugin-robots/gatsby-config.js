731826c96faa5fd8f897761042c4d1f9b6c5bf1c
John Gerrard Holland <john_holland1@brown.edu>
2022-10-26 11:04:33 -0400

    feat: add plugin to set up robots.txt for each site, using netlify specifics

---
+++ packages/project-portal-plugin-robots/gatsby-config.js
@@ -0,0 +1,27 @@
+const { NODE_ENV, CONTEXT: NETLIFY_ENV = NODE_ENV } = process.env
+
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: "*" }],
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+          "deploy-preview": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
+  ],
+}
