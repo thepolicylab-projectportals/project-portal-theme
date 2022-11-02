be2a84ac05aa1f65f7c5ce6e7ac470b1422c49d2
John Gerrard Holland <john_holland1@brown.edu>
2022-11-02 09:57:06 -0400

    refactor: move getSiteUrl function to utils

--- packages/gatsby-theme-project-portal/gatsby-config.js
+++ packages/gatsby-theme-project-portal/gatsby-config.js
@@ -1,43 +1,5 @@
 const { withDefaults } = require(`./utils/default-options`)
-
-function getSiteUrl() {
-  if (process.env.NETLIFY) {
-    // we're running on Netlify
-    switch (process.env.CONTEXT) {
-      case "production":
-        return process.env.URL
-      case "deploy-preview":
-        return process.env.DEPLOY_PRIME_URL
-      case "branch-deploy":
-        return process.env.DEPLOY_PRIME_URL
-      case "development":
-        return `http://localhost:${process.env.PORT ?? "8000"}`
-      default:
-        console.error(`context unknown: ${process.env.CONTEXT}`)
-        process.exit(1)
-    }
-  } else if (process.env.CI) {
-    // We're running on GitHub
-    return `http://localhost:9000`
-  } else {
-    // We're probably running locally.
-    // The user might have set a port, but if not, we need a sensible default.
-    let defaultPort
-    console.log(`We're probably running locally. ${process.env.NODE_ENV}`)
-    switch (process.env.NODE_ENV) {
-      case "development":
-        defaultPort = 8000
-        break
-      case "production":
-        defaultPort = 9000
-        break
-      default:
-        console.warn(`NODE_ENV value unknown: ${process.env.NODE_ENV}`)
-        defaultPort = 8000
-    }
-    return `http://localhost:${process.env.PORT ?? defaultPort}`
-  }
-}
+const { getSiteUrl } = require(`./utils/getSiteUrl`)
 
 module.exports = (themeOptions) => {
   const themeOptionsWithDefaults = withDefaults(themeOptions)
