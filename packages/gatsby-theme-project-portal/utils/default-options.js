931b1b238cc92f76a9eff0413d28415037818fd3
John Gerrard Holland <john_holland1@brown.edu>
2022-11-10 17:45:00 -0500

    fix: replace logical or with nullish coalescing operator

--- packages/gatsby-theme-project-portal/utils/default-options.js
+++ packages/gatsby-theme-project-portal/utils/default-options.js
@@ -4,15 +4,15 @@ const defaultStaticText = require("./default-static-text.json")
 
 function withDefaults(themeOptions) {
   return {
-    siteTitle: themeOptions.siteTitle || `Gatsby Theme Project Portal`,
-    shortTitle: themeOptions.shortTitle || `Project Portal`,
+    siteTitle: themeOptions.siteTitle ?? `Gatsby Theme Project Portal`,
+    shortTitle: themeOptions.shortTitle ?? `Project Portal`,
     faviconPath:
-      themeOptions.faviconPath || `${__dirname}/../images/default-icon.png`,
-    showDevBanner: themeOptions.showDevBanner || true,
+      themeOptions.faviconPath ?? `${__dirname}/../images/default-icon.png`,
+    showDevBanner: themeOptions.showDevBanner ?? true,
     staticText: lodash.merge(defaultStaticText, themeOptions.staticText),
     themeImageDirectory:
-      themeOptions.themeImageDirectory || "./content/theme-image",
-    pages: themeOptions.pages || [
+      themeOptions.themeImageDirectory ?? "./content/theme-image",
+    pages: themeOptions.pages ?? [
       {
         name: "Home",
         link: "/",
@@ -20,7 +20,7 @@ function withDefaults(themeOptions) {
       },
     ],
     tailwindConfig:
-      themeOptions.tailwindConfig || require(`../src/styles/tailwind.presets`),
+      themeOptions.tailwindConfig ?? require(`../src/styles/tailwind.presets`),
     // The ReCAPTCHA site key defaults to the standard example key provided by Google for the v2 recaptcha.
     // If the user wants to provide NO site key, not even the default, then they can set the themeOptions:
     // themeOptions: { recaptchaSiteKey: "" }
