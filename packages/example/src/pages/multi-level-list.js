3e425b9b29f32a76e60d9d7271c20a551483d934
John Gerrard Holland <john_holland1@brown.edu>
2022-11-30 15:40:04 -0500

    test: update examples of multi-level lists

--- packages/example/src/pages/multi-level-list.js
+++ packages/example/src/pages/multi-level-list.js
@@ -2,8 +2,20 @@ import React from "react"
 import { Layout } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/layouts"
 import { MarkdownText } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
 
-const markdownMultiLevelList =
-  "- Collaborator.\n  - Details.\n  - Details.\n  - Details.\n    - Details.\n    - Details.\n    - Details.\n- Expertise.\n"
+const oneLevelList =
+  "# One-Level List\n- Collaborator.\n- Expertise.\n- Awesome.\n\nParagraph Text."
+
+const twoLevelList =
+  "# Two-Level List\n- Collaborator.\n  - Details.\n  - Details.\n  - Details.\n- Expertise.\n\nParagraph Text."
+
+const threeLevelList =
+  "# Three-Level List\n- Collaborator.\n  - L2.\n    - Details.\n    - Details.\n      - Details.\n      - Details.\n      - Details.\n- Expertise.\n\nParagraph Text."
+
+const fiveLevelList =
+  "# Five-Level List\n- L1.\n  - L2.\n    - L3.\n      - L4.\n        - Details.\n        - Details.\n        - Details.\n- Expertise.\n\nParagraph Text."
+
+const oneLevelListWithAdditionalParagraph =
+  '# One-Level List With Additional Paragraph\n- Collaborator.\n\n  And another line under "Collaborator"\n- Expertise.\n- Awesome.\n\nParagraph Text.'
 
 const LayoutTest = () => {
   return (
@@ -12,8 +24,12 @@ const LayoutTest = () => {
       description={"Example page showing multi-level-lists"}
       title={"Multi-Level Lists"}
     >
-      <MarkdownText text={markdownMultiLevelList} />
-      <p>Paragraph text</p>
+      <MarkdownText text={oneLevelList} />
+      <MarkdownText text={twoLevelList} />
+      <MarkdownText text={threeLevelList} />
+      <MarkdownText text={fiveLevelList} />
+
+      <MarkdownText text={oneLevelListWithAdditionalParagraph} />
     </Layout>
   )
 }
