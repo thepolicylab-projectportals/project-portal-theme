1e7a3de90e1edaddd5e708cb1845720eb4df7e4a
John Gerrard Holland <john_holland1@brown.edu>
2023-02-03 16:21:06 -0500

    feat: switch to using new static text node for staticText hook

--- packages/gatsby-theme-project-portal/src/hooks/useStaticText.tsx
+++ packages/gatsby-theme-project-portal/src/hooks/useStaticText.tsx
@@ -1,65 +1,61 @@
 import { graphql, useStaticQuery } from "gatsby"
 
 export const useStaticText = () => {
-  const {
-    projectPortalConfig: { staticText },
-  } = useStaticQuery(graphql`
+  const { projectPortalStaticText } = useStaticQuery(graphql`
     query StaticTextQuery {
-      projectPortalConfig {
-        staticText {
-          about {
-            header
-            aims {
-              title
-              text
-            }
-            faq {
-              title
-              text
-            }
-            accessibility
-          }
-          bottom_banner {
+      projectPortalStaticText {
+        about {
+          header
+          aims {
+            title
             text
+          }
+          faq {
+            title
+            text
+          }
+          accessibility
+        }
+        bottom_banner {
+          text
+          link
+        }
+        main_contact_text {
+          ongoingText
+          completeText
+        }
+        open {
+          lede
+          pageName
+          title
+        }
+        ongoing {
+          lede
+          pageName
+          title
+        }
+        footer {
+          copyright
+          heading {
             link
-          }
-          main_contact_text {
-            ongoingText
-            completeText
-          }
-          open {
-            lede
-            pageName
             title
           }
-          ongoing {
-            lede
-            pageName
+          links {
+            link
             title
           }
-          footer {
-            copyright
-            heading {
-              link
-              title
-            }
-            links {
-              link
-              title
-            }
-          }
-          contact {
-            lede
-            title
-          }
-          completed {
-            lede
-            pageName
-            title
-          }
+        }
+        contact {
+          lede
+          title
+        }
+        completed {
+          lede
+          pageName
+          title
         }
       }
     }
   `)
-  return staticText
+  return projectPortalStaticText
 }
