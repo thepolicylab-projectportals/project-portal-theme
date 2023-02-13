664aec70f3e41678f4f70302f02c33bba0dca903
John Gerrard Holland <john_holland1@brown.edu>
2023-02-13 17:00:07 -0500

    refactor: update path to cardPage directory

--- packages/project-portal-content-netlify/utils/default-options.js
+++ packages/project-portal-content-netlify/utils/default-options.js
@@ -4,6 +4,7 @@ function withDefaults(pluginOptions) {
     projectPath: pluginOptions.projectPath || "./content/project",
     contactPath: pluginOptions.contactPath || "./content/contact",
     topicPath: pluginOptions.topicPath || "./content/topic",
+    cardPagePath: pluginOptions.cardPagePath || "./content/card-page",
   }
 }
 
