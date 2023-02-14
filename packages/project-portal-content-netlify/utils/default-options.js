fa987bf29315722a84447eec8161a234ca9f0404
John Gerrard Holland <john_holland1@brown.edu>
2023-02-14 09:36:55 -0500

    feat: add path to config directory in theme options

--- packages/project-portal-content-netlify/utils/default-options.js
+++ packages/project-portal-content-netlify/utils/default-options.js
@@ -1,11 +1,12 @@
 function withDefaults(pluginOptions) {
   return {
     ...pluginOptions,
-    projectPath: pluginOptions.projectPath || "./content/project",
-    contactPath: pluginOptions.contactPath || "./content/contact",
-    topicPath: pluginOptions.topicPath || "./content/topic",
-    cardPagePath: pluginOptions.cardPagePath || "./content/card-page",
-    pagePath: pluginOptions.pagePath || "./content/page",
+    configPath: pluginOptions?.configPath || "./content/config",
+    projectPath: pluginOptions?.projectPath || "./content/project",
+    contactPath: pluginOptions?.contactPath || "./content/contact",
+    topicPath: pluginOptions?.topicPath || "./content/topic",
+    cardPagePath: pluginOptions?.cardPagePath || "./content/card-page",
+    pagePath: pluginOptions?.pagePath || "./content/page",
   }
 }
 
