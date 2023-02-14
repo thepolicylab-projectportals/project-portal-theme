f8ff9fa11bcd5cbbf1e34e6bf6e80f070be0587d
John Gerrard Holland <john_holland1@brown.edu>
2023-02-14 09:36:37 -0500

    feat: add cms editing for site-metadata

--- packages/project-portal-content-netlify/src/cms/cms.js
+++ packages/project-portal-content-netlify/src/cms/cms.js
@@ -428,6 +428,38 @@ CMS.init({
           },
         ],
       },
+      {
+        name: "config",
+        label: "Configuration",
+        files: [
+          {
+            name: "siteMetadata",
+            label: "Site Metadata",
+            file: "content/config/site-metadata.json",
+            fields: [
+              {
+                name: "templateKey",
+                widget: "hidden",
+                default: "siteMetadata",
+              },
+              {
+                name: "title",
+                label: "Title",
+                widget: "string",
+                required: true,
+                hint: "Site title, used in metadata",
+              },
+              {
+                name: "description",
+                label: "Description",
+                widget: "string",
+                required: false,
+                hint: "Site description, passed to search engines",
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
 })
