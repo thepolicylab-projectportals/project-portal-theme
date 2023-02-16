911e9b727b729018293d1d57dc4d8ec9cdd656c9
Josh <joshua_lu1@brown.edu>
2023-02-16 11:50:05 -0500

    refactor: color styling

--- packages/gatsby-theme-project-portal/src/components/SearchBar.tsx
+++ packages/gatsby-theme-project-portal/src/components/SearchBar.tsx
@@ -15,7 +15,7 @@ export const SearchBar: FunctionComponent<SearchBarProps> = ({
         {label}
       </label>
       <input
-        className="rounded border-neutral-300"
+        className="rounded border-gray-300 hover:border-gray-400"
         style={{height: "62%"}}
         type="text"
         aria-label="Search"
