e5df61d860f82e3b65ce461506bc089c841f336f
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-05-02 16:27:26 -0400

    fix: prepare tarball

--- packages/project-portal-content-netlify/src/cms/cms-utils.js
+++ packages/project-portal-content-netlify/src/cms/cms-utils.js
@@ -1,29 +1,16 @@
-if (typeof window !== "undefined") {
-  // add admin.css
-  const styles =
-    "[data-slate-editor] {-webkit-user-modify: read-write !important;" + "}\n"
-  const css = document.createElement("style")
-  css.type = "text/css"
-  if (css.styleSheet) {
-    css.styleSheet.cssText = styles
-  } else {
-    css.appendChild(document.createTextNode(styles))
-  }
+if (typeof window !== 'undefined') {
 
-  document.head.appendChild(css)
+  // add admin.css
+  const link = document.createElement('link')
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  link.href = '/admin/admin.css'
+  document.head.appendChild(link)
 
   if (process.env.NETLIFY_SITE_URL) {
-    window.localStorage.setItem("netlifySiteURL", process.env.NETLIFY_SITE_URL)
-  }
-
-  // Log netlifySiteURL if editing on localhost
-  if (
-    window.location.hostname === "localhost" &&
-    window.localStorage.getItem("netlifySiteURL")
-  ) {
-    console.log(
-      `%cnetlifySiteURL: ${window.localStorage.getItem("netlifySiteURL")}`,
-      "color: hotpink; font-size: 15px"
+    window.localStorage.setItem(
+      'netlifySiteURL', process.env.NETLIFY_SITE_URL
     )
   }
+
 }
