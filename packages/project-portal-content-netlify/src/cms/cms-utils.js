81b8d4adfb09852a523984d64f5b179866a08efa
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-05-08 14:56:49 -0400

    fix: insert css tag directly into header
    admin.css file is not accessible from the site since the site is referencing from the public folder and unable to reach the node_modules folder

--- packages/project-portal-content-netlify/src/cms/cms-utils.js
+++ packages/project-portal-content-netlify/src/cms/cms-utils.js
@@ -1,16 +1,13 @@
-if (typeof window !== 'undefined') {
-
-  // add admin.css
-  const link = document.createElement('link')
-  link.type = 'text/css'
-  link.rel = 'stylesheet'
-  link.href = '/admin/admin.css'
-  document.head.appendChild(link)
+if (typeof window !== "undefined") {
+  // add styles to header of the cms page
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style>[data-slate-editor] {
+    -webkit-user-modify: read-write !important;
+    }</style>`
+  )
 
   if (process.env.NETLIFY_SITE_URL) {
-    window.localStorage.setItem(
-      'netlifySiteURL', process.env.NETLIFY_SITE_URL
-    )
+    window.localStorage.setItem("netlifySiteURL", process.env.NETLIFY_SITE_URL)
   }
-
 }
