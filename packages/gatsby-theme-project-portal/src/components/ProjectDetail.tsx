d0400bc74311fd073f0a2c72d2a3d89882c26e4a
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-05-08 11:44:33 -0400

    feat: make icons bigger on mobile

--- packages/gatsby-theme-project-portal/src/components/ProjectDetail.tsx
+++ packages/gatsby-theme-project-portal/src/components/ProjectDetail.tsx
@@ -88,7 +88,7 @@ export const ProjectDetailLayout: FunctionComponent<
         <div className="py-16 p-responsive bg-primary">
           <div className="flex flex-col justify-between m-responsive lg:flex-row">
             <div className="w-auto">
-              <h1 className="text-h3 sm:text-h2 w-full font-bold leading-h2 text-white lg:w-4/5">
+              <h1 className="text-h2 md:text-h2 w-full font-bold leading-h2 text-white lg:w-4/5">
                 {question}
               </h1>
               {
--- packages/gatsby-theme-project-portal/src/components/ShareProject.tsx
+++ packages/gatsby-theme-project-portal/src/components/ShareProject.tsx
@@ -17,23 +17,27 @@ export const ShareProject: React.FC = () => {
 
   return (
     <div className="mt-8 w-72 lg:mt-0">
-      <h4 className="text-h4 font-bold text-white">Share this project</h4>
-      <div className="flex gap-1 mt-1 flex-nowrap place-content-start">
-        <FacebookShareButton url={url} resetButtonStyle={true}>
-          <FaFacebookSquare className="text-2xl text-white" />
+      <h4 className="font-bold text-white text-2xl">Share this project</h4>
+      <div className="flex gap-6 mt-1 flex-nowrap place-content-start lg:gap-1">
+        <FacebookShareButton
+          url={url}
+          resetButtonStyle={true}
+          className="shared"
+        >
+          <FaFacebookSquare className="text-5xl text-white lg:text-2xl" />
         </FacebookShareButton>
         <TwitterShareButton url={url} resetButtonStyle={true}>
-          <FaTwitterSquare className="text-2xl text-white" />
+          <FaTwitterSquare className="text-5xl text-white lg:text-2xl" />
         </TwitterShareButton>
         <LinkedinShareButton url={url} resetButtonStyle={true}>
-          <FaLinkedin className="text-2xl text-white" />
+          <FaLinkedin className="text-5xl text-white lg:text-2xl" />
         </LinkedinShareButton>
         <EmailShareButton
           resetButtonStyle={true}
           url={url}
           subject={"Sharing a wonderful project opportunity!"}
         >
-          <FaEnvelopeSquare className="text-2xl text-white" />
+          <FaEnvelopeSquare className="text-5xl text-white lg:text-2xl" />
         </EmailShareButton>
       </div>
     </div>
