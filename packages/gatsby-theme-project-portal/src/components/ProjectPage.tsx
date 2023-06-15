153c3d2a7e09da5d1429c05da34bdc1081f9fc0e
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-06-15 12:25:29 -0400

    fix: specify type of multivalue
    type dispatch is not assignable to type multivalue error with the onchange event is fixed with this change

--- packages/gatsby-theme-project-portal/src/components/ProjectPage.tsx
+++ packages/gatsby-theme-project-portal/src/components/ProjectPage.tsx
@@ -3,7 +3,7 @@ import { Cards, CardProps } from "../components"
 import { HeaderWithImage } from "./HeaderWithImage"
 import { BackIcon } from "./BackIcon"
 import { ForwardIcon } from "./ForwardIcon"
-import Select from "react-select"
+import Select, { MultiValue } from "react-select"
 import * as JsSearch from "js-search"
 import { SearchBar } from "./SearchBar"
 
@@ -172,7 +172,7 @@ export const ProjectPage = ({
     setHasNext(pageEnd < displayProjects.length)
   }, [list]) // triggered when list is changed
 
-  const [selectedOptions, setSelectedOptions] = useState([])
+  const [selectedOptions, setSelectedOptions] = useState<MultiValue<any>>([])
 
   useEffect(() => {
     const sortedList = [...allProjects]
