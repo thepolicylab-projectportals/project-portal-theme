e3626de12f74bded6c0b880a09fe27a73b9d91ef
John Gerrard Holland <john_holland1@brown.edu>
2023-07-17 11:44:09 -0400

    docs: add initial BackIcon story

---
+++ packages/gatsby-theme-project-portal/src/components/BackIcon.stories.js
@@ -0,0 +1,12 @@
+import { BackIcon } from "./BackIcon"
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: "Theme/BackIcon",
+  component: BackIcon,
+  tags: ["autodocs"],
+  argTypes: {},
+}
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {}
