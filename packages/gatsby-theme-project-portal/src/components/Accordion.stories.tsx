8232dd9d2a66f2460b30a64c5e10cce36f75bb7c
John Gerrard Holland <john_holland1@brown.edu>
2023-07-20 16:12:54 -0400

    refactor: convert accordion story to TSC

--- packages/gatsby-theme-project-portal/src/components/Accordion.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/Accordion.stories.tsx
@@ -1,14 +1,12 @@
+import * as React from "react"
+import { Meta, StoryFn } from "@storybook/react"
 import { Accordion } from "./Accordion"
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: "Theme/Accordion",
   component: Accordion,
-  tags: ["autodocs"],
-  argTypes: {},
-}
+} as Meta
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = {
-  args: { title: "A title", text: "The text" },
-}
+export const Primary: StoryFn = (args) => <Accordion {...args} />
+Primary.args = { title: "A title", text: "The text" }
