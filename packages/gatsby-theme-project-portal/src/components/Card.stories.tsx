4492e23e15e9387bee9e5f3d5a1420624de7b5e5
John Gerrard Holland <john_holland1@brown.edu>
2023-07-25 11:31:04 -0400

    docs: update cardstories

--- packages/gatsby-theme-project-portal/src/components/Card.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/Card.stories.tsx
@@ -1,15 +1,22 @@
 import type { Meta, StoryObj } from "@storybook/react"
-import { Card } from "./Card"
+import { Card, CardProps } from "./Card"
 
 const meta: Meta<typeof Card> = {
   component: Card,
+  tags: ["autodocs"],
+  argTypes: {
+    status: {
+      options: ["open", "ongoing", "completed"],
+      control: { type: "radio" },
+    },
+  },
 }
 
 export default meta
 
-type Story = StoryObj<typeof Card>
+type Story = StoryObj<typeof CardProps>
 
-export const Primary: Story = {
+export const Open: Story = {
   args: {
     slug: "test-project",
     title: "Test Project",
@@ -17,9 +24,24 @@ export const Primary: Story = {
     status: "open",
     agency: "Sample Agency",
     topics: [{ slug: "topic", title: "Topic" }],
+    created: "2022-03-04",
+    opportunityCloses: "2022-03-04",
+    startDate: "2022-01-03",
+    endDate: "2022-03-04",
+    lastModified: "2022-05-27T16:34:04.000Z",
   },
 }
-// opportunityCloses: Date("2022-03-04"),
-// startDate: "2022-01-03",
-// endDate: "2022-03-04",
-// lastModified: "2022-05-27T16:34:04.000Z",
+
+export const Ongoing: Story = {
+  args: {
+    ...Open.args,
+    status: "ongoing",
+  },
+}
+
+export const Completed: Story = {
+  args: {
+    ...Open.args,
+    status: "completed",
+  },
+}
