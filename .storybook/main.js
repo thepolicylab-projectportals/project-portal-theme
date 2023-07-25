52b8991e9eb280a88a30d839e2d45248e986f0b1
John Gerrard Holland <john_holland1@brown.edu>
2023-07-25 08:29:28 -0400

    docs: add fixed gatsby config from https://github.com/storybookjs/storybook/issues/22065

--- .storybook/main.js
+++ .storybook/main.js
@@ -24,5 +24,21 @@ const config = {
   babel: {
     presets: ["@babel/preset-react", "@babel/preset-typescript"],
   },
+  webpackFinal: async (config) => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[2].exclude = [
+      /node_modules\/(?!(gatsby|gatsby-script)\/)/,
+    ]
+
+    // Remove core-js to prevent issues with Storybook
+    config.module.rules[2].exclude = [/core-js/]
+    // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+    config.module.rules[2].use[0].options.plugins.push(
+      require.resolve("babel-plugin-remove-graphql-queries")
+    )
+
+    config.resolve.mainFields = ["browser", "module", "main"]
+    return config
+  },
 }
 export default config
