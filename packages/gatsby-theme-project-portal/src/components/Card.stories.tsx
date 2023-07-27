80437172d7820c122891c50b056c748a88fe680c
John Gerrard Holland <john_holland1@brown.edu>
2023-07-27 11:51:09 -0400

    refactor: use exported data objects rather than story args (fix a problem with the types of the data)

--- packages/gatsby-theme-project-portal/src/components/Card.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/Card.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react"
 import { Card } from "./Card"
 import { statusArgType } from "./Story.utilities"
 
-const exampleTopics = [
+export const exampleTopics = [
   { slug: "topic", title: "Topic" },
   { slug: "short-topic", title: "Short Topic" },
   { slug: "medium-topic-name", title: "Medium Topic Name" },
@@ -12,6 +12,7 @@ const exampleTopics = [
 const meta: Meta<typeof Card> = {
   component: Card,
   tags: ["autodocs"],
+  includeStories: /^[A-Z]/,
   argTypes: {
     ...statusArgType,
     topics: {
@@ -29,80 +30,90 @@ export default meta
 
 type Story = StoryObj<typeof Card>
 
+export const primaryData = {
+  slug: "test-project",
+  title: "Test Project",
+  question: "Test Question?",
+  status: "open",
+  agency: "Sample Agency",
+  topics: [{ slug: "topic", title: "Topic" }],
+  opportunityCloses: new Date("2022-03-04"),
+  startDate: new Date("2022-01-03"),
+  endDate: new Date("2022-03-04"),
+  lastModified: new Date("2022-05-27T16:34:04.000Z"),
+  created: new Date("2022-03-04"),
+}
+
 export const Primary: Story = {
-  args: {
-    slug: "test-project",
-    title: "Test Project",
-    question: "Test Question?",
-    status: "open",
-    agency: "Sample Agency",
-    topics: [{ slug: "topic", title: "Topic" }],
-    opportunityCloses: new Date("2022-03-04"),
-    startDate: new Date("2022-01-03"),
-    endDate: new Date("2022-03-04"),
-    lastModified: new Date("2022-05-27T16:34:04.000Z"),
-    created: new Date("2022-03-04"),
-  },
+  args: primaryData,
+}
+
+export const openData = {
+  ...primaryData,
+  status: "open",
+  question: "Is this project open?",
+  opportunityCloses: null,
 }
 
 export const Open: Story = {
-  args: {
-    ...Primary.args,
-    status: "open",
-    question: "Is this project open?",
-    opportunityCloses: null,
-  },
+  args: openData,
 }
 
+export const openNoDateData = {
+  ...primaryData,
+  status: "open",
+  question: "Should this project have a date?",
+  opportunityCloses: null,
+}
 export const OpenNoDate: Story = {
-  args: {
-    ...Primary.args,
-    status: "open",
-    question: "Should this project have a date?",
-    opportunityCloses: null,
-  },
+  args: openNoDateData,
 }
 
+export const ongoingData = {
+  ...primaryData,
+  question: "Why are we still doing this project?",
+  status: "ongoing",
+}
 export const Ongoing: Story = {
-  args: {
-    ...Primary.args,
-    question: "Why are we still doing this project?",
-    status: "ongoing",
-  },
+  args: ongoingData,
+}
+export const ongoingNoDateData = {
+  ...primaryData,
+  status: "ongoing",
+  question: "Should this project finish sometime?",
+  startDate: null,
 }
 export const OngoingNoDate: Story = {
-  args: {
-    ...Primary.args,
-    status: "ongoing",
-    question: "Should this project finish sometime?",
-    startDate: null,
-  },
+  args: ongoingNoDateData,
 }
 
+export const completedData = {
+  ...primaryData,
+  question: "Why did we stop doing this?",
+  status: "completed",
+}
 export const Completed: Story = {
-  args: {
-    ...Primary.args,
-    question: "Why did we stop doing this?",
-    status: "completed",
-  },
+  args: completedData,
 }
 
+export const completedNoDateData = {
+  ...primaryData,
+  status: "completed",
+  question: "Is this actually finished?",
+  endDate: null,
+}
 export const CompletedNoDate: Story = {
-  args: {
-    ...Primary.args,
-    status: "completed",
-    question: "Is this actually finished?",
-    endDate: null,
-  },
+  args: completedNoDateData,
 }
 
+export const longTopicsListData = {
+  ...primaryData,
+  topics: [
+    { slug: "short-topic", title: "Short Topic" },
+    { slug: "medium-topic-name", title: "Medium Topic Name" },
+    { slug: "extremely-long-topic-name", title: "Extremely Long Topic Name" },
+  ],
+}
 export const LongTopicsList: Story = {
-  args: {
-    ...Primary.args,
-    topics: [
-      { slug: "short-topic", title: "Short Topic" },
-      { slug: "medium-topic-name", title: "Medium Topic Name" },
-      { slug: "extremely-long-topic-name", title: "Extremely Long Topic Name" },
-    ],
-  },
+  args: longTopicsListData,
 }
