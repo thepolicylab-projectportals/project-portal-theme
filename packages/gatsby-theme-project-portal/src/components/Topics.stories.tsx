1513cb6f4691ad52ff21f449c07f883892c84216
John Gerrard Holland <john_holland1@brown.edu>
2023-07-28 08:44:23 -0400

    docs: add Topics stories

---
+++ packages/gatsby-theme-project-portal/src/components/Topics.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Topics } from "./Topics"
+
+export const exampleTopics = [
+  { slug: "topic", title: "Topic" },
+  { slug: "short-topic", title: "Short Topic" },
+  { slug: "medium-topic-name", title: "Medium Topic Name" },
+  { slug: "extremely-long-topic-name", title: "Extremely Long Topic Name" },
+]
+
+export const topicsArgType = {
+  topics: {
+    options: Object.keys(exampleTopics),
+    control: {
+      type: "multi-select",
+      labels: exampleTopics.map((topic) => topic.title),
+    },
+    mapping: exampleTopics,
+  },
+}
+
+const meta: Meta<typeof Topics> = {
+  component: Topics,
+  tags: ["autodocs"],
+  includeStories: /^[A-Z]/,
+  argTypes: { ...topicsArgType },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Topics>
+
+export const Primary: Story = {
+  args: { topics: exampleTopics },
+}
