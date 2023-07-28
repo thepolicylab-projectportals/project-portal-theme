c03b9a879e3f084f9fe99f7d3f8bbc1464e63b36
John Gerrard Holland <john_holland1@brown.edu>
2023-07-28 13:09:31 -0400

    chore: remove extra console.log

--- packages/gatsby-theme-project-portal/wrapPageElement.js
+++ packages/gatsby-theme-project-portal/wrapPageElement.js
@@ -2,7 +2,8 @@ import React from "react"
 import { NewLayout } from "./src/layouts/NewLayout"
 
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
-const wrapPageElement = ({ element, props }) =>
-  console.log(props) || <NewLayout {...props}>{element}</NewLayout>
+const wrapPageElement = ({ element, props }) => (
+  <NewLayout {...props}>{element}</NewLayout>
+)
 
 export default wrapPageElement
