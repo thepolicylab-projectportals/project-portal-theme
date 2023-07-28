bba345af5d3c4764e88674deee262c66b2290ff3
John Gerrard Holland <john_holland1@brown.edu>
2023-07-28 16:47:32 -0400

    chore: delete useSiteMetadata from hooks

--- packages/gatsby-theme-project-portal/src/hooks/index.tsx
+++ packages/gatsby-theme-project-portal/src/hooks/index.tsx
@@ -1,4 +1,3 @@
 export { useProjectPortalConfig } from "./useProjectPortalConfig"
-export { useSiteMetadata } from "./useSiteMetadata"
 export { useStaticText } from "./useStaticText"
 export { Head } from "./Head"
