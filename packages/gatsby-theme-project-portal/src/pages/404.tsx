7937214baa52ab6f25f320bc795ee8567f8b30c9
John Gerrard Holland <john_holland1@brown.edu>
2023-07-28 14:26:58 -0400

    fix: add minimum query to 404 page

--- packages/gatsby-theme-project-portal/src/pages/404.tsx
+++ packages/gatsby-theme-project-portal/src/pages/404.tsx
@@ -1,23 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
-import { Layout } from "../layouts"
+import { graphql, Link } from "gatsby"
 
 export default () => {
   return (
-    <Layout title="Error 404" description="Page not found">
-      <div className="w-full py-20 px-8 lg:px-16 xl:px-24 lg:w-2/3">
-        <h1 className="text-3xl font-extrabold leading-tight lg:text-5xl">
-          Error 404
-        </h1>
-        <h3 className="text-lg font-medium lg:text-xl">
-          The page you're looking for doesn't exist.
-          <br />
-          <br />
-          <Link to="/" className="text-link">
-            Go back to homepage
-          </Link>
-        </h3>
-      </div>
-    </Layout>
+    <div className="w-full py-20 px-8 lg:px-16 xl:px-24 lg:w-2/3">
+      <h1 className="text-3xl font-extrabold leading-tight lg:text-5xl">
+        Error 404
+      </h1>
+      <h3 className="text-lg font-medium lg:text-xl">
+        The page you're looking for doesn't exist.
+        <br />
+        <br />
+        <Link to="/" className="text-link">
+          Go back to homepage
+        </Link>
+      </h3>
+    </div>
   )
 }
+
+export const query = graphql`
+  query q404 {
+    projectPortalConfig {
+      ...LayoutQuery
+    }
+    site {
+      ...HeadData
+    }
+  }
+`
