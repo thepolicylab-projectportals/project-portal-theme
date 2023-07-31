8e9d2594fcd5e249d55eab18575ff5c3955d6508
John Gerrard Holland <john_holland1@brown.edu>
2023-07-31 08:58:53 -0400

    docs: add image to BottomBanner story

--- packages/gatsby-theme-project-portal/src/components/BottomBanner.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/BottomBanner.stories.tsx
@@ -1,11 +1,24 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { BottomBannerLayout } from "./BottomBanner"
 
-import { emptyGatsbyImageData } from "./Story.utilities"
+import { IGatsbyImageData } from "gatsby-plugin-image"
 
 const meta: Meta<typeof BottomBannerLayout> = {
   component: BottomBannerLayout,
   tags: ["autodocs"],
+  includeStories: /^[A-Z]/,
+}
+
+export const bottomBannerLogo: IGatsbyImageData = {
+  layout: "fixed",
+  images: {
+    fallback: {
+      src: "image/ccv-logo.png",
+    },
+    sources: [],
+  },
+  width: 160,
+  height: 83,
 }
 
 export default meta
@@ -17,6 +30,6 @@ export const Primary: Story = {
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     link: "https://ccv.brown.edu",
     linkId: null,
-    image: emptyGatsbyImageData,
+    image: bottomBannerLogo,
   },
 }
