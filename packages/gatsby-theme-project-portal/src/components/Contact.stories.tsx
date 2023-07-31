641e080cc695727dd5c5f6ec9dba2d917a952ed0
John Gerrard Holland <john_holland1@brown.edu>
2023-07-31 08:59:07 -0400

    docs: add image to Contact story

--- packages/gatsby-theme-project-portal/src/components/Contact.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/Contact.stories.tsx
@@ -1,15 +1,40 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { Contact } from "./Contact"
-import { emptyGatsbyImageData } from "./Story.utilities"
+import { IGatsbyImageData } from "gatsby-plugin-image"
 
 const meta: Meta<typeof Contact> = {
   component: Contact,
   tags: ["autodocs"],
+  includeStories: /^[A-Z]/,
 }
 
 export default meta
 
 type Story = StoryObj<typeof Contact>
+
+export const contactImage: IGatsbyImageData = {
+  layout: "fixed",
+  images: {
+    fallback: {
+      src: "image/torso.png",
+    },
+    sources: [],
+  },
+  width: 100,
+  height: 100,
+}
+
+export const contactImageYogi: IGatsbyImageData = {
+  layout: "fixed",
+  images: {
+    fallback: {
+      src: "image/yogi.jpg",
+    },
+    sources: [],
+  },
+  width: 100,
+  height: 100,
+}
 
 export const Primary: Story = {
   args: {
@@ -18,10 +43,14 @@ export const Primary: Story = {
     employer: "Employer",
     email: "some-email@example.com",
     showEmail: true,
-    image: emptyGatsbyImageData,
+    image: contactImage,
   },
 }
 
 export const NoEmail: Story = {
   args: { ...Primary.args, showEmail: false },
+}
+
+export const NoImage: Story = {
+  args: { ...Primary.args, image: null },
 }
