5acc741f4127c5906c1ca729b7a92cc5fd4f7f5e
John Gerrard Holland <john_holland1@brown.edu>
2023-07-31 09:04:55 -0400

    docs: add image to SiteTitle

--- packages/gatsby-theme-project-portal/src/components/SiteTitle.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/SiteTitle.stories.tsx
@@ -2,10 +2,24 @@ import type { Meta, StoryObj } from "@storybook/react"
 import { SiteTitle } from "./SiteTitle"
 
 import { emptyGatsbyImageData } from "./Story.utilities"
+import { IGatsbyImageData } from "gatsby-plugin-image"
 
 const meta: Meta<typeof SiteTitle> = {
   component: SiteTitle,
   tags: ["autodocs"],
+  includeStories: /^[A-Z]/,
+}
+
+export const siteTitleLogo: IGatsbyImageData = {
+  layout: "fixed",
+  images: {
+    fallback: {
+      src: "image/logo-square.png",
+    },
+    sources: [],
+  },
+  width: 64,
+  height: 74,
 }
 
 export default meta
@@ -14,7 +28,7 @@ type Story = StoryObj<typeof SiteTitle>
 
 export const Primary: Story = {
   args: {
-    image: emptyGatsbyImageData,
+    image: siteTitleLogo,
     title: "The Site Title",
   },
 }
