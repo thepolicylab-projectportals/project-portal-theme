f4e341ba09b245ba66420c4beded6e6b83844746
John Gerrard Holland <john_holland1@brown.edu>
2023-07-31 12:47:35 -0400

    chore: remove duplicated tags, move twitter summary to top

--- packages/gatsby-theme-project-portal/src/hooks/Head.tsx
+++ packages/gatsby-theme-project-portal/src/hooks/Head.tsx
@@ -6,30 +6,20 @@ export function Head({ location, params, data, pageContext }) {
 
   return (
     <>
+      <title>{data.page?.title}</title>
+      <meta name="twitter:card" content="summary" />
       <meta property="og:type" content="website" />
-
       <html lang={data.site.siteMetadata.locale} />
       <meta property="og:locale" content={data.site.siteMetadata.locale} />
-
       <meta property="og:site_name" content={data.site.siteMetadata.title} />
-
-      <title>{data.page?.title}</title>
       <meta name="og:title" content={data.page?.title} />
-      <meta name="twitter:title" content={data.page?.title} />
-
       <meta name="description" content={data.page?.description} />
       <meta property="og:description" content={data.page?.description} />
-      <meta name="twitter:description" content={data.page?.description} />
-
       <meta
         property="og:url"
         content={`${data.site.siteMetadata.siteUrl}${location.pathname}`}
       />
-
       <meta property="og:image" content={socialShareImage} />
-      <meta name="twitter:image" content={socialShareImage} />
-
-      <meta name="twitter:card" content="summary" />
     </>
   )
 }
