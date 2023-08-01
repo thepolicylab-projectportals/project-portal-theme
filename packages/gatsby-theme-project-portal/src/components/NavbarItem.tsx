a233f19ab058abb916b6bdf69a704216ea0c754f
John Gerrard Holland <john_holland1@brown.edu>
2023-08-01 16:08:52 -0400

    fix: add react fragment

--- packages/gatsby-theme-project-portal/src/components/NavbarItem.tsx
+++ packages/gatsby-theme-project-portal/src/components/NavbarItem.tsx
@@ -11,18 +11,20 @@ export const NavbarItem: FunctionComponent<NavbarItemProps> = ({
   isActive,
 }) => {
   return (
-    <li className="nav-item">
-      <span className="flex items-center p-5 leading-snug text-white hover:opacity-75 xl:text-black xl:px-3 xl:py-2">
-        {isActive ? (
-          <span className="text-nav pb-1 ml-2 font-bold border-b-2 border-white xl:border-primary">
-            {name}
-          </span>
-        ) : (
-          <span className="text-nav pb-1 ml-2 border-b-2 hover:border-primary xl:border-transparent">
-            {name}
-          </span>
-        )}
-      </span>
-    </li>
+    <>
+      <li className="nav-item">
+        <span className="flex items-center p-5 leading-snug text-white hover:opacity-75 xl:text-black xl:px-3 xl:py-2">
+          {isActive ? (
+            <span className="text-nav pb-1 ml-2 font-bold border-b-2 border-white xl:border-primary">
+              {name}
+            </span>
+          ) : (
+            <span className="text-nav pb-1 ml-2 border-b-2 hover:border-primary xl:border-transparent">
+              {name}
+            </span>
+          )}
+        </span>
+      </li>
+    </>
   )
 }
