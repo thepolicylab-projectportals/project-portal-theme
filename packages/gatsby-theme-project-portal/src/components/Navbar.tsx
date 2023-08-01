92b282f8b348c41ffcd515e1d7d84f39cf0bc665
John Gerrard Holland <john_holland1@brown.edu>
2023-08-01 07:48:35 -0400

    fix: make menu disappear when clicked

--- packages/gatsby-theme-project-portal/src/components/Navbar.tsx
+++ packages/gatsby-theme-project-portal/src/components/Navbar.tsx
@@ -62,7 +62,10 @@ export const NavbarLayout: FunctionComponent<NavbarLayoutProps> = ({
             <ul className="flex flex-col list-none xl:flex-row xl:ml-auto">
               {pages.map(({ name, link, show }, i) =>
                 show ? (
-                  <Link to={link ? link : "#"}>
+                  <Link
+                    to={link ? link : "#"}
+                    onClick={() => setNavbarOpen(false)}
+                  >
                     <NavbarItem
                       key={"nav" + i}
                       name={name}
