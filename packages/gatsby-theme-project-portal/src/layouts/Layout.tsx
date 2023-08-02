c16fe89dfc74d8992740333267dc8e949580c123
John Gerrard Holland <john_holland1@brown.edu>
2023-08-02 06:43:59 -0400

    chore: use new Navbar in Layout

--- packages/gatsby-theme-project-portal/src/layouts/Layout.tsx
+++ packages/gatsby-theme-project-portal/src/layouts/Layout.tsx
@@ -1,26 +1,51 @@
 import React, { FunctionComponent, ReactNode } from "react"
 import { graphql } from "gatsby"
 import { Footer, BottomBanner, DevelopmentBanner, Navbar } from "../components"
+import { IGatsbyImageData, getImage } from "gatsby-plugin-image"
 
 interface LayoutProps {
   path: string
   title: string
   description: string
   children: ReactNode
-  data: { projectPortalConfig: { showDevBanner?: boolean } }
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+      }
+    }
+    projectPortalConfig: {
+      showDevBanner?: boolean
+      pages: {
+        name: string
+        link: string
+        show: boolean
+      }[]
+    }
+    navbarLogo?: IGatsbyImageData
+  }
 }
 
 export const Layout: FunctionComponent<LayoutProps> = ({
   path,
   data: {
-    projectPortalConfig: { showDevBanner },
+    site: {
+      siteMetadata: { title: siteTitle },
+    },
+    projectPortalConfig: { showDevBanner, pages },
+    navbarLogo,
   },
   children,
 }) => {
   return (
     <div className="w-full mx-0 bg-white border-0 xl:container xl:p-0 xl:mx-auto xl:border-l xl:border-r xl:border-gray-200 flex flex-col min-h-screen">
       {showDevBanner && <DevelopmentBanner />}
-      <Navbar activePage={path} />
+      <Navbar
+        title={siteTitle}
+        image={navbarLogo ? getImage(navbarLogo) : null}
+        pages={pages}
+        activePage={path}
+      />
       <div className="flex-1">{children}</div>
       <BottomBanner />
       <Footer />
@@ -30,8 +55,27 @@ export const Layout: FunctionComponent<LayoutProps> = ({
 
 export const query = graphql`
   fragment LayoutData on Query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     projectPortalConfig {
       showDevBanner
+      pages {
+        link
+        name
+        show
+      }
+    }
+    navbarLogo: file(
+      name: { eq: "navbar" }
+      extension: { in: ["png", "jpg", "jpeg"] }
+      sourceInstanceName: { eq: "themeImages" }
+    ) {
+      childImageSharp {
+        gatsbyImageData(height: 64)
+      }
     }
   }
 `
