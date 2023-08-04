5aae271761423c05a8f25e71f769b9db24b9f38e
John Gerrard Holland <john_holland1@brown.edu>
2023-08-04 07:53:52 -0400

    chore: remove unused hooks

--- packages/gatsby-theme-project-portal/src/hooks/index.tsx
+++ packages/gatsby-theme-project-portal/src/hooks/index.tsx
@@ -1,4 +1,1 @@
-export { useProjectPortalConfig } from "./useProjectPortalConfig"
-export { useSiteMetadata } from "./useSiteMetadata"
-export { useStaticText } from "./useStaticText"
 export { Head } from "./Head"
