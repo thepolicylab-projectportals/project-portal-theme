8ca77aec7e427ee5ab28a1aeb2a4b160d23ba753
John Gerrard Holland <john_holland1@brown.edu>
2023-08-07 15:53:43 +0200

    revert: removal of state data

--- packages/gatsby-theme-project-portal/src/components/Cards.tsx
+++ packages/gatsby-theme-project-portal/src/components/Cards.tsx
@@ -11,7 +11,14 @@ export const Cards: FunctionComponent<CardsProps> = ({ nodes }) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-3 xl:mx-6 gap-4 justify-self-center place-items-stretch">
       {nodes.map((item, i) => (
         <article>
-          <Link to={`/project/${item.slug}`} className="h-full">
+          <Link
+            to={`/project/${item.slug}`}
+            state={{
+              current: i,
+              items: nodes.map((item) => `/${item.slug}`),
+            }}
+            className="h-full"
+          >
             <Card key={"card_" + item.slug} {...item} />
           </Link>
         </article>
