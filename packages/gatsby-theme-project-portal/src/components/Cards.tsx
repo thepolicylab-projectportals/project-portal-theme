d03883f7bbc45843d8dbab0cd6493aaf4b91d507
John Gerrard Holland <john_holland1@brown.edu>
2023-08-07 15:14:12 +0200

    fix: card heights (but why?)

--- packages/gatsby-theme-project-portal/src/components/Cards.tsx
+++ packages/gatsby-theme-project-portal/src/components/Cards.tsx
@@ -8,15 +8,9 @@ export interface CardsProps {
 
 export const Cards: FunctionComponent<CardsProps> = ({ nodes }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-3 xl:mx-6 gap-4 justify-self-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-3 xl:mx-6 gap-4 justify-self-center place-items-stretch">
       {nodes.map((item, i) => (
-        <Link
-          to={`/project/${item.slug}`}
-          state={{
-            current: i,
-            items: nodes.map((item) => `/${item.slug}`),
-          }}
-        >
+        <Link to={`/project/${item.slug}`}>
           <Card key={"card_" + item.slug} {...item} />
         </Link>
       ))}
