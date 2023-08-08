49237ba18c7b2a7d816e64416419eff3af7e4d97
John Gerrard Holland <john_holland1@brown.edu>
2023-08-08 21:07:39 +0200

    chore: remove extra className which doesn't seem to have an effect

--- packages/gatsby-theme-project-portal/src/components/Cards.tsx
+++ packages/gatsby-theme-project-portal/src/components/Cards.tsx
@@ -8,7 +8,7 @@ export interface CardsProps {
 
 export const Cards: FunctionComponent<CardsProps> = ({ nodes }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-3 xl:mx-6 gap-4 justify-self-center place-items-stretch">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-3 xl:mx-6 gap-4 justify-self-center">
       {nodes.map((item, i) => (
         <article>
           <Link
