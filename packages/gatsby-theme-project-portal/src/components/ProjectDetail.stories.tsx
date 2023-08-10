c7f2d37d3314419474935de380603b3447f36b04
John Gerrard Holland <john_holland1@brown.edu>
2023-08-10 17:30:47 +0200

    docs: add viewport sizes to the most prominent responsive components

--- packages/gatsby-theme-project-portal/src/components/ProjectDetail.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/ProjectDetail.stories.tsx
@@ -92,3 +92,33 @@ export const Minimum: Story = {
     lastModified: new Date("2022-05-27T16:34:04.000Z"),
   },
 }
+
+export const TailwindXSWindow: Story = {
+  args: Primary.args,
+  parameters: { viewport: { defaultViewport: "tailwindXS" } },
+}
+
+export const TailwindSMWindow: Story = {
+  args: Primary.args,
+  parameters: { viewport: { defaultViewport: "tailwindSM" } },
+}
+
+export const TailwindMDWindow: Story = {
+  args: Primary.args,
+  parameters: { viewport: { defaultViewport: "tailwindMD" } },
+}
+
+export const TailwindLGWindow: Story = {
+  args: Primary.args,
+  parameters: { viewport: { defaultViewport: "tailwindLG" } },
+}
+
+export const TailwindXLWindow: Story = {
+  args: Primary.args,
+  parameters: { viewport: { defaultViewport: "tailwindXL" } },
+}
+
+export const Tailwind2XLWindow: Story = {
+  args: Primary.args,
+  parameters: { viewport: { defaultViewport: "tailwind2XL" } },
+}
