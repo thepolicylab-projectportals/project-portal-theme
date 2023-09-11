210034ed04c8e137cbe6e2e5951b419395f572cc
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-09-11 10:55:12 -0400

    feat: search modal
    todo: remove this and replace with search page

--- packages/gatsby-theme-project-portal/src/components/SearchModal.tsx
+++ packages/gatsby-theme-project-portal/src/components/SearchModal.tsx
@@ -1,8 +1,70 @@
 import React, { useEffect, useState } from "react"
 import { SearchBar } from "./SearchBar"
+import * as JsSearch from "js-search"
+import { CardProps } from "./Card"
+import { graphql } from "gatsby"
+import { TopicType } from "./Topics"
 
+export interface ModalProps {
+  // Card Details
+  slug: string
+  title: string
+  question: string
+  status: string
+  agency: string
+  topics: TopicType[]
+
+  // Page Details
+  data: {
+    site: { siteMetadata: { title } }
+    generalPage: {
+      pageName: string
+      title: string
+      aims: {
+        title: string
+        text: string
+      }[]
+      faq: {
+        title: string
+        text: string
+      }[]
+    }
+  }
+}
+
+export const query = graphql`
+  fragment CardDetails on Project {
+    slug
+    title
+    question
+    status
+    agency
+    topics {
+      ...TopicDetails
+    }
+    opportunityCloses
+    startDate
+    endDate
+    lastModified
+    created
+  }
+`
 export default function SearchModal() {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = React.useState(false)
+  const [searchQuery, setSearchQuery] = useState([])
+
+  let search = new JsSearch.Search("slug")
+
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      search.addDocuments(filteredProjects)
+      let searchResults = search.search(searchQuery)
+      if (searchResults.length > 0) {
+        filteredProjects = searchResults
+      }
+    }
+    //setDisplayProjects will trigger an updated display
+  }, [searchQuery])
 
   return (
     <>
@@ -23,7 +85,7 @@ export default function SearchModal() {
                 <div className="flex items-center justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <SearchBar
                     label={"Search"}
-                    onChange={(e) => console.log(e)}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                   />
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
