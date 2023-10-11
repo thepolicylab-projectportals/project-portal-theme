b5c465f9b1926876e8d5ad46df4fcd5464b6c1e7
John Gerrard Holland <john_holland1@brown.edu>
2023-10-11 09:41:38 -0400

    refactor: use new PageLayoutNested in Layout component, adding new tests

--- packages/gatsby-theme-project-portal/src/layouts/Layout.tsx
+++ packages/gatsby-theme-project-portal/src/layouts/Layout.tsx
@@ -1,33 +1,35 @@
 import React, { FunctionComponent, ReactNode } from "react"
 import { graphql } from "gatsby"
-import { Footer, BottomBanner, DevelopmentBanner, Navbar } from "../components"
 import { ImageDataLike } from "gatsby-plugin-image"
+import { PageLayoutNested } from "../components/PageLayoutNested"
 
 export interface LayoutProps {
   path: string
   children: ReactNode
-  data: {
-    site: {
-      siteMetadata: {
-        title: string
+  data?: {
+    site?: {
+      siteMetadata?: {
         showDevBanner?: boolean
-        pages: {
-          name: string
-          link: string
-          show: boolean
-        }[]
-        staticText: {
-          bottomBanner: {
+        staticText?: {
+          navbar?: {
+            pages?: {
+              name: string
+              link: string
+              show: boolean
+            }[]
+            title?: string
+          }
+          bottomBanner?: {
             text: string
             link: string
           }
-          footer: {
-            copyright: string
-            links: {
+          footer?: {
+            copyright?: string
+            links?: {
               title: string
               link: string
             }[]
-            heading: {
+            heading?: {
               title: string
               link: string
             }
@@ -43,47 +45,30 @@ export interface LayoutProps {
 
 export const Layout: FunctionComponent<LayoutProps> = ({
   path,
-  data: {
-    site: {
-      siteMetadata: {
-        title: siteTitle,
-        showDevBanner,
-        pages,
-        staticText: { bottomBanner, footer },
-      },
-    },
-    navbarLogo,
-    bottomBannerLogo,
-    footerLogo,
-  },
+  data,
   children,
 }) => {
   return (
-    <div className="w-full mx-0 bg-white border-0 xl:container xl:p-0 xl:mx-auto xl:border-l xl:border-r xl:border-gray-200 flex flex-col min-h-screen">
-      {showDevBanner && <DevelopmentBanner />}
-      <Navbar
-        title={siteTitle}
-        image={navbarLogo}
-        pages={pages}
-        activePage={path}
-      />
-      <div className="flex-1">{children}</div>
-      <BottomBanner
-        text={bottomBanner.text}
-        link={bottomBanner.link}
-        linkId={"bottomBannerLink"}
-        image={bottomBannerLogo}
-      />
-      <Footer
-        heading={footer.heading}
-        copyright={footer.copyright}
-        links={footer.links}
-        image={{
-          imageData: footerLogo,
-          altText: siteTitle + " logo",
-        }}
-      />
-    </div>
+    <PageLayoutNested
+      children={children}
+      devBanner={{ show: data?.site?.siteMetadata?.showDevBanner }}
+      navbar={{
+        ...data?.site?.siteMetadata?.staticText?.navbar,
+        image: data?.navbarLogo,
+        activePage: path,
+      }}
+      bottomBanner={{
+        ...data?.site?.siteMetadata?.staticText?.bottomBanner,
+        image: data?.bottomBannerLogo,
+      }}
+      footer={{
+        ...data?.site?.siteMetadata?.staticText?.footer,
+        image: {
+          imageData: data?.footerLogo,
+          altText: data?.site?.siteMetadata?.title + " logo",
+        },
+      }}
+    />
   )
 }
 
@@ -91,14 +76,16 @@ export const query = graphql`
   fragment LayoutData on Query {
     site {
       siteMetadata {
-        title
         showDevBanner
-        pages {
-          link
-          name
-          show
-        }
         staticText {
+          navbar {
+            pages {
+              link
+              name
+              show
+            }
+            title
+          }
           bottomBanner {
             text
             link
