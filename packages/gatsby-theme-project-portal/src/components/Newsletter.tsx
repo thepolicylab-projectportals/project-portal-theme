3f4c4532f988a2949385da184fca08a164da678f
John Gerrard Holland <john_holland1@brown.edu>
2023-10-13 12:10:26 -0400

    fix: remove extra props on <anchor> so it doesn't always open a new page

--- packages/gatsby-theme-project-portal/src/components/Newsletter.tsx
+++ packages/gatsby-theme-project-portal/src/components/Newsletter.tsx
@@ -14,7 +14,7 @@ export const Newsletter: FunctionComponent<NewsletterProps> = ({
       {link ? (
         <div className="w-full p-8 mb-8 bg-gray-100">
           {text ? <h4 className="text-h4">{text}</h4> : null}
-          <a href={link} target="_blank" rel="noopener">
+          <a href={link}>
             <button className="btn">Subscribe</button>
           </a>
         </div>
