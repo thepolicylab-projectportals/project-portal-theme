b0d339264afac28c4210558ed985e9b497f461e2
John Gerrard Holland <john_holland1@brown.edu>
2023-10-26 18:05:05 -0400

    chore: ignore unused global export because it's used implicitly by Gatsby

--- packages/gatsby-theme-project-portal/gatsby-browser.js
+++ packages/gatsby-theme-project-portal/gatsby-browser.js
@@ -2,4 +2,6 @@ import "./src/styles/global.css"
 
 import CustomLayout from "./wrapPageElement"
 
+// The unused constant is implicitly used by Gatsby, so we ignore the linting error it causes
+// noinspection JSUnusedGlobalSymbols
 export const wrapPageElement = CustomLayout
--- packages/gatsby-theme-project-portal/gatsby-ssr.js
+++ packages/gatsby-theme-project-portal/gatsby-ssr.js
@@ -1,3 +1,5 @@
 import CustomLayout from "./wrapPageElement"
 
+// The unused constant is implicitly used by Gatsby, so we ignore the linting error it causes
+// noinspection JSUnusedGlobalSymbols
 export const wrapPageElement = CustomLayout
