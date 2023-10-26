1d72b3bc746ff66568a165ad21460caae9975e2b
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-10-26 10:46:22 -0400

    refactor: fix SearchBar autofocus

--- packages/gatsby-theme-project-portal/src/components/SearchBar.tsx
+++ packages/gatsby-theme-project-portal/src/components/SearchBar.tsx
@@ -1,30 +1,32 @@
-import React, {
-  ChangeEventHandler,
-  Dispatch,
-  FunctionComponent,
-  SetStateAction,
-} from "react"
+import React, { FunctionComponent } from "react"
+import { Label } from "./Label"
 
 interface SearchBarProps {
+  id: string
   label: string
   onChange: any
+  placeholder: string
+  autoFocus?: boolean
 }
 
 export const SearchBar: FunctionComponent<SearchBarProps> = ({
+  id,
   label,
   onChange,
+  placeholder,
+  autoFocus,
 }) => {
   return (
     <>
-      <label id="search-label" className="font-bold" htmlFor="filter">
-        {label}
-      </label>
+      <Label id={id} label={label} />
       <input
+        autoFocus={autoFocus}
         className="rounded border-gray-300 hover:border-gray-400"
         style={{ height: "62%" }}
         type="text"
+        id={id}
         aria-label="Search"
-        placeholder="Type to filter posts..."
+        placeholder={placeholder}
         onChange={onChange}
       />
     </>
