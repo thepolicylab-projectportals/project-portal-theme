b26fae091c3c72b9a7b6f64b5ea63f5f6655bed1
John Gerrard Holland <john_holland1@brown.edu>
2023-10-26 18:03:17 -0400

    chore: remove unused import

--- packages/gatsby-theme-project-portal/src/layouts/CardPageLayout.stories.tsx
+++ packages/gatsby-theme-project-portal/src/layouts/CardPageLayout.stories.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import type { Meta, StoryObj } from "@storybook/react"
 import { CardPageLayout } from "./CardPageLayout"
 import { exampleCards } from "../components/Cards.stories"
