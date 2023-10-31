0a485b4adf711e767311ca61bc7fde63b1e344da
John Gerrard Holland <john_holland1@brown.edu>
2023-10-31 16:50:10 -0400

    test: move site title stories bits into Navbar

--- packages/gatsby-theme-project-portal/src/components/Navbar.stories.tsx
+++ packages/gatsby-theme-project-portal/src/components/Navbar.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { Navbar } from "./Navbar"
-import * as SiteTitleStories from "./SiteTitle.stories"
+import { loadImage } from "./Story.utilities"
 
 const meta: Meta<typeof Navbar> = {
   component: Navbar,
@@ -12,11 +12,13 @@ export default meta
 
 type Story = StoryObj<typeof Navbar>
 
+export const siteTitleLogo = loadImage("image/logo-square.png", 64, 74, "fixed")
+
 export const Primary: Story = {
   args: {
     title: "The Site Title",
-    image: SiteTitleStories.siteTitleLogo,
-    activePage: "Home",
+    image: siteTitleLogo,
+    activePage: "/",
     pages: [
       { name: "Home", link: "/", show: true },
       { name: "Open", link: "/open/", show: true },
@@ -27,10 +29,6 @@ export const Primary: Story = {
       { name: "FAQ", link: "/faq/", show: true },
     ],
   },
-}
-
-export const OpenPageIsActive: Story = {
-  args: { ...Primary.args, activePage: "/open/" },
 }
 
 export const NoLogo: Story = {
@@ -79,7 +77,7 @@ export const Tailwind2XLWindow: Story = {
 export const EmptyStrings: Story = {
   args: {
     title: "",
-    image: SiteTitleStories.siteTitleLogo,
+    image: siteTitleLogo,
     activePage: "",
     pages: [],
   },
