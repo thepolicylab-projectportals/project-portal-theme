2cfc17ce7d9ed5506ec3bd3f2c48bbbe0e833459
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-11-09 14:46:24 -0500

    feat: fetch data from static folder, tell gatsby not to run fetch on build
    Co-Authored-By: bradford roarr <49485134+broarr@users.noreply.github.com>

--- packages/gatsby-theme-project-portal/src/components/SiteSearchWrapper.tsx
+++ packages/gatsby-theme-project-portal/src/components/SiteSearchWrapper.tsx
@@ -1,111 +1,38 @@
-import React, {
-  FunctionComponent,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react"
-import { graphql, useStaticQuery } from "gatsby"
+import React, { FunctionComponent, useEffect, useState } from "react"
+import lunr from "lunr"
 import SiteSearch from "./SiteSearch"
-import { createSearchIndex } from "../utils"
 
 export interface SearchWrapperProps {
   siteUrl: string
 }
-const siteSearchQuery = graphql`
-  query searchNodes {
-    allProject {
-      nodes {
-        slug
-        question
-        title
-        summary
-        status
-        opportunityCloses
-        startDate
-        endDate
-        lastModified
-        agency
-        topics {
-          ...TopicDetails
-        }
-        deliverable
-        purpose
-        expertise
-        requirement
-        keyDates
-        priorResearch
-        statusOfData
-        fundingInfo
-        emailContent
-        mainContact {
-          name
-          title
-          employer
-          email
-        }
-        projectTeam {
-          name
-          title
-          employer
-          email
-        }
-        faq {
-          text
-          title
-        }
-      }
-    }
-    allGeneralPage {
-      nodes {
-        slug
-        lede
-        faq {
-          text
-          title
-        }
-        aims {
-          text
-          title
-        }
-        title
-      }
-    }
-  }
-`
 
 export const SearchWrapper: FunctionComponent<SearchWrapperProps> = ({
   siteUrl,
 }: SearchWrapperProps) => {
-  console.log("search page has loaded")
+  const [idx, setIdx] = useState()
+  const [db, setDb] = useState()
+  useEffect(() => {
+    const getIndex = async () => {
+      const savedIndex = await (await fetch("/lunr-index.json")).json()
+      const documents = await (await fetch("/documents.json")).json()
 
-  // const queryData = useMemo(() => {
-  //   const data = useStaticQuery(siteSearchQuery)
-  //   return { ...data, siteUrl }
-  // }, [siteSearchQuery, siteUrl])
-  // const searchIndex = useMemo(() => createSearchIndex(queryData), [queryData])
+      setIdx(lunr.Index.load(savedIndex))
+      // this is a function which grabs the slug from the original documents
+      // lunr tosses this info for SPEED
+      setDb(
+        documents.reduce(function (acc, document) {
+          acc[document.slug] = document
+          return acc
+        }, {})
+      )
+    }
 
-  /* this is a function which grabs the slug from the original documents; lunr tosses this info for SPEED
-  // const db = documents.reduce(function (acc, document) {
-  //   acc[document.slug] = document
-  //   return acc
-  // }, {})
-   */
+    // Because Gatsby runs this on build, we need to say to ignore getting the json files
+    // if the window is undefined (not running in the browser)
+    // see: https://www.gatsbyjs.com/docs/debugging-html-builds/
+    if (typeof window !== `undefined`) getIndex().catch(console.error)
+  }, [])
 
-  // useEffect(() => {
-  //   // declare the data fetching function
-  //   const fetchData = async () => {
-  //     const data = await import("/static/lunrIndex.json")
-  //     let index = (lunr.Index.load(JSON.parse(data)))
-  //   }
-  //
-  //   // call the function
-  //   fetchData()
-  //     // make sure to catch any error
-  //     .catch(console.error)
-  // }, [])
-
-  return <div>{/*<SiteSearch siteUrl={siteUrl} index={index} db={db} />*/}</div>
+  return <div>{<SiteSearch siteUrl={siteUrl} index={idx} db={db} />}</div>
 }
 export default SearchWrapper
