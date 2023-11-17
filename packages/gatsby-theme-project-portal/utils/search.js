6832732e8ab4ecbff0a109edcc41efecda5d04b4
Heather Yu <35639529+hetd54@users.noreply.github.com>
2023-11-17 17:09:21 -0500

    feat: implement our own stopwordfilter

--- packages/gatsby-theme-project-portal/utils/search.js
+++ packages/gatsby-theme-project-portal/utils/search.js
@@ -43,6 +43,128 @@ function createSearchIndex(searchNodes) {
     }
   })
 
+  // ask lunr to ignore these words within a search
+  const newStopWordFilter = lunr.generateStopWordFilter([
+    "a",
+    "able",
+    "across",
+    "after",
+    "all",
+    "almost",
+    "also",
+    "am",
+    "among",
+    "an",
+    "and",
+    "any",
+    "are",
+    "as",
+    "at",
+    "be",
+    "because",
+    "been",
+    "but",
+    "by",
+    "can",
+    "cannot",
+    "could",
+    "dear",
+    "did",
+    "do",
+    "does",
+    "either",
+    "else",
+    "ever",
+    "every",
+    "for",
+    "from",
+    "get",
+    "got",
+    "had",
+    "has",
+    "have",
+    "he",
+    "her",
+    "hers",
+    "him",
+    "his",
+    "how",
+    "however",
+    "i",
+    "if",
+    "in",
+    "into",
+    "is",
+    "it",
+    "its",
+    "just",
+    "least",
+    "let",
+    "like",
+    "likely",
+    "may",
+    "me",
+    "might",
+    "most",
+    "must",
+    "my",
+    "neither",
+    "no",
+    "nor",
+    "not",
+    "of",
+    "off",
+    "often",
+    "on",
+    "only",
+    "or",
+    "other",
+    "our",
+    "own",
+    "rather",
+    "said",
+    "say",
+    "says",
+    "she",
+    "should",
+    "since",
+    "so",
+    "some",
+    "than",
+    "that",
+    "the",
+    "their",
+    "them",
+    "then",
+    "there",
+    "these",
+    "they",
+    "this",
+    "tis",
+    "to",
+    "too",
+    "twas",
+    "us",
+    "wants",
+    "was",
+    "we",
+    "were",
+    "what",
+    "when",
+    "where",
+    "which",
+    "while",
+    "who",
+    "whom",
+    "why",
+    "will",
+    "with",
+    "would",
+    "yet",
+    "you",
+    "your",
+  ])
+
   let index = lunr(function () {
     this.ref("slug") // identifier
     // fields to search
@@ -66,15 +188,8 @@ function createSearchIndex(searchNodes) {
 
     // allow access to the position of the found word in the field
     this.metadataWhitelist = ["position"]
-    /* allow stop words (see https://github.com/olivernn/lunr.js/blob/master/lunr.js)
-           we have 'about' as one of the slugs, which spurred this decision
-           We can implement our own stopWordFilter:
-                    var idx = lunr(function () {
-                      // normal index definition
-                      this.pipeline.after(lunr.stopWordFilter, myStopWordFilter)
-                       this.pipeline.remove(lunr.stopWordFilter)
-                    })
-        */
+
+    this.pipeline.after(lunr.stopWordFilter, newStopWordFilter)
     this.pipeline.remove(lunr.stopWordFilter)
 
     documents.forEach(function (doc) {
