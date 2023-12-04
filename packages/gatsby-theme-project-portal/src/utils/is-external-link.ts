8d733f689466f716c6018d2e16a42a841cad8d45
John Gerrard Holland <john_holland1@brown.edu>
2023-12-04 12:21:45 -0500

    docs: update comment on is-external-link file

--- packages/gatsby-theme-project-portal/src/utils/is-external-link.ts
+++ packages/gatsby-theme-project-portal/src/utils/is-external-link.ts
@@ -1,5 +1,5 @@
-// Copied from https://github.com/sindresorhus/is-absolute-url/blob/3ab19cc2e599a03ea691bcb8a4c09fa3ebb5da4f/index.js
-// and from https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-link/src/is-local-link.js
+// Inspired by
+// https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-link/src/is-local-link.js
 export const STANDARD_LOCAL_LINK = /^\/$|^\/.*\/$/
 export const ABSOLUTE_URL_REGEX = /^([a-zA-Z]+:)?\/\//
 export const ABSOLUTE_URL_REGEX_WITH_PROTOCOL = /^[a-zA-Z]+:\/\//
