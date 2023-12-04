0bed4d9030dc77352e646289a1a12cb6188566f2
John Gerrard Holland <john_holland1@brown.edu>
2023-12-04 17:29:50 -0500

    style: make external link symbol smaller

--- packages/gatsby-theme-project-portal/src/components/Navbar.tsx
+++ packages/gatsby-theme-project-portal/src/components/Navbar.tsx
@@ -42,8 +42,8 @@ export const InactivePageNavItem: FunctionComponent<{ name }> = ({ name }) => {
 export const ExternalLinkSymbol = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width="16"
+    height="16"
     viewBox="0 0 16 16"
     {...props}
   >
