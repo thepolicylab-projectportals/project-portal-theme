1a10fdc7d29e9bc7efcbfabd2a933139ab4522f0
John Gerrard Holland <john_holland1@brown.edu>
2023-12-04 12:20:16 -0500

    refactor: update CMS to allow new kinds of links

--- packages/gatsby-theme-project-portal/src/utils/is-external-link.ts
+++ packages/gatsby-theme-project-portal/src/utils/is-external-link.ts
@@ -1,9 +1,8 @@
 // Copied from https://github.com/sindresorhus/is-absolute-url/blob/3ab19cc2e599a03ea691bcb8a4c09fa3ebb5da4f/index.js
 // and from https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-link/src/is-local-link.js
-// const ABSOLUTE_URL_REGEX = /^[a-zA-Z][a-zA-Z\d+\-.]*?:/
-const STANDARD_LOCAL_LINK = /^\/$|^\/.*\/$/
-const ABSOLUTE_URL_REGEX = /^([a-zA-Z]+:)?\/\//
-const ABSOLUTE_URL_REGEX_WITH_PROTOCOL = /^[a-zA-Z]+:\/\//
+export const STANDARD_LOCAL_LINK = /^\/$|^\/.*\/$/
+export const ABSOLUTE_URL_REGEX = /^([a-zA-Z]+:)?\/\//
+export const ABSOLUTE_URL_REGEX_WITH_PROTOCOL = /^[a-zA-Z]+:\/\//
 const isAbsolute = (path) => ABSOLUTE_URL_REGEX.test(path)
 
 export const isLocalLink = (path: string) => {
--- packages/project-portal-content-decap/src/cms/cms.js
+++ packages/project-portal-content-decap/src/cms/cms.js
@@ -2,6 +2,10 @@ import CMS from "decap-cms-app"
 
 // Add custom styles to the index.html page
 import "./cms-utils"
+import {
+  ABSOLUTE_URL_REGEX_WITH_PROTOCOL,
+  STANDARD_LOCAL_LINK,
+} from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/utils/is-external-link"
 
 // The following configuration is merged with the configuration from the site's config.yml file
 // (if it exists in the site's directory static/admin/config.yml)
@@ -555,10 +559,14 @@ CMS.init({
                         name: "link",
                         widget: "string",
                         pattern: [
-                          /^\/$|^\/.*\/$/,
-                          "Should start and end with a slash.",
-                        ], // from
-                        // https://regexr.com/7hpn2
+                          new RegExp(
+                            STANDARD_LOCAL_LINK.source +
+                              "|" +
+                              ABSOLUTE_URL_REGEX_WITH_PROTOCOL.source
+                          ),
+                          "Internal links start and end with a single slash, e.g. '/open/'.\n" +
+                            "External links include the protocol and hostname, like 'https://ccv.brown.edu/'",
+                        ],
                       },
                       { name: "show", widget: "boolean" },
                     ],
