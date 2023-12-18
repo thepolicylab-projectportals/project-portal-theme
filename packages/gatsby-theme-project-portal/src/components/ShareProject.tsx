6398458d946043686a6cab698e2d58a567741a16
John Gerrard Holland <john_holland1@brown.edu>
2023-12-18 15:42:46 -0500

    accessibility: make share this project section into a div rather than an h4

--- packages/gatsby-theme-project-portal/src/components/ShareProject.tsx
+++ packages/gatsby-theme-project-portal/src/components/ShareProject.tsx
@@ -18,8 +18,9 @@ export const ShareProject: React.FC = () => {
   return (
     <div className="mt-8 w-72 lg:mt-0">
       <h4 className="font-bold text-white text-xl lg:text-h4">
+      <div className="font-bold text-white text-xl lg:text-h4">
         Share this project
-      </h4>
+      </div>
       <div className="flex gap-6 mt-1 flex-nowrap place-content-start text-5xl text-white lg:text-2xl lg:gap-1">
         <FacebookShareButton
           url={url}
